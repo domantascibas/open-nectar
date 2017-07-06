1bcc29d1eb6a59bd32954eeca0c797132242843d
paplaukias <domantas.cibas@gmail.com>
2017-07-06 19:58:14 +0300

    added state machine to select one of 4 available operational modes

---
+++ inc/utils.h
@@ -0,0 +1,13 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+enum mode_operational {
+	MODE_DEFAULT,
+	MODE_BOOST,
+	MODE_AWAY,
+	MODE_NO_GRID
+};
+
+#endif
+
+// *******************************Nectar Sun Copyright © Nectar Sun 2017*************************************   
