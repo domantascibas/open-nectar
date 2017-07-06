aee24a254ef201e5ee74999b16dec2676cb5f52b
paplaukias <domantas.cibas@gmail.com>
2017-07-06 19:55:42 +0300

    added 2 namespaces for main functions and one for testing

--- inc/modes.h
+++ inc/modes.h
@@ -1,6 +1,20 @@
 #ifndef MODES_H
 #define MODES_H
 
+namespace device {
+	void set_mode(int);
+	int get_mode();
+	
+	void run(int);
+}
+
+//test functions. Delete for release
+namespace device_tests {
+	void set_temperature_max(double);
+	void set_temperature_current(double);
+	void set_pv_available(bool);
+}
+
 #endif
 
 // *******************************Nectar Sun Copyright © Nectar Sun 2017*************************************   
