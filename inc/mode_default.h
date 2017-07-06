7c1bb59f16599a6938bb6c709d1f55cb2f71b5fe
paplaukias <domantas.cibas@gmail.com>
2017-07-06 19:56:52 +0300

    simple DEFAULT mode state machine

--- inc/mode_default.h
+++ inc/mode_default.h
@@ -1,6 +1,16 @@
 #ifndef MODE_DEFAULT_H
 #define MODE_DEFAULT_H
 
+enum mode_power_source {
+	TURN_ON_GRID,
+	TURN_ON_PV,
+	TURN_OFF_ALL
+};
+
+namespace mode_default {
+	int run(double, double, bool);
+}
+
 #endif
 
 // *******************************Nectar Sun Copyright © Nectar Sun 2017*************************************   
