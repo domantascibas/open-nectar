e9bc68adb931c012cb63ce8a1f3b0ddaa352dad6
paplaukias <domantas.cibas@gmail.com>
2017-07-07 13:32:36 +0300

    moved the mode_power_source enum from mode_default.h to utils.h, so it can be reused by all other operational modes

--- inc/mode_default.h
+++ inc/mode_default.h
@@ -1,12 +1,6 @@
 #ifndef MODE_DEFAULT_H
 #define MODE_DEFAULT_H
 
-enum mode_power_source {
-    TURN_ON_GRID,
-    TURN_ON_PV,
-    TURN_OFF_ALL
-};
-
 namespace mode_default {
     uint8_t run(double, double, double, bool);
 }
--- inc/utils.h
+++ inc/utils.h
@@ -1,6 +1,12 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+enum mode_power_source {
+    TURN_ON_GRID,
+    TURN_ON_PV,
+    TURN_OFF_ALL
+};
+
 enum mode_operational {
     MODE_DEFAULT,
     MODE_BOOST,
