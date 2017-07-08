a4266bd4a3e54da5b4e5148aaa404fea549c0ae3
paplaukias <domantas.cibas@gmail.com>
2017-07-08 18:21:37 +0300

    added tests for comms between main board and power board

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include "mbed.h"
 #include "rtos.h"
 #include "modes.h"
+#include "comms.h"
 #include "utils.h"
+#include "pin_names.h"
 
 DigitalOut led1(LED1);
 Thread device_update;
 Thread blinky;
+Serial pc(USBTX, USBRX);
 
 extern DigitalOut relay_sun_on;
 extern DigitalOut relay_grid_on;
+extern Serial comms_power;
+extern Serial comms_esp;
 
 void print_thread(void);
 
@@ -55,13 +60,24 @@ void print_thread(void) {
 }
 
 int main() {
-    relay_sun_on = false;
-    relay_grid_on = false;
+    //comms_power.printf("ready to go\n\r");
+    //device::set_relays(TURN_OFF_ALL);
     
-    printf("\n\n\r[ STARTING ]\n\r");
+    //printf("\n\n\r[ STARTING ]\n\r");
+    //power_board::start();
+    //wait(1);
     //blinky.start(&print_thread);
-    default_mode_test();
-
+    //default_mode_test();
+    //power_board::stop();
+    
+    //pc.printf("Received: %5.3f\n\r", power_board::get_voltage());
+    pc.printf("Starting\n\r");
+    //comms_power.putc(0x56);
+    double voltage = 120.50;
+    int voltage_int = voltage * 2049;
+    comms_power.putc(voltage_int);
+    //comms_power.printf("%f", voltage);
+    pc.printf("done\n\r");
     while(1) {
         __WFI();
     }
