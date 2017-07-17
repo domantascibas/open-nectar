c1e23e0a06df9b1fb209422ab43c129425754261
paplaukias <domantas.cibas@gmail.com>
2017-07-17 12:46:07 +0300

    created a separate object to save nectar data shareable between different functions

--- inc/data.h
+++ inc/data.h
@@ -7,22 +7,56 @@ struct data_collection {
     float temperature_scheduled;
     float temperature_moment;       //water temperature
     
-    float moment_sun_watt;
+    float sun_power_moment;
     float sun_voltage;
     float sun_current;
     float pwm_duty;
     
     float device_temperature;
-    float capacitor_tempearature;
+    float capacitor_temperature;
 
     bool sun_relay_on;
     bool grid_relay_on;
     bool transistor_overheat_on;
+    bool pv_available;
     
     uint8_t current_mode;
-} nectar_data;
+};
 
-namespace data {
+namespace data_tests {
+    void set_stats(float, float, float, float, float, float, float, bool, bool, bool);
+    
+    void set_temperature_max(float);
+    void set_temperature_min(float);
+    void set_temperature_scheduled(float);
+    void set_temperature_moment(float);
+    void set_sun_power_moment(float);
+    void set_sun_voltage(float);
+    void set_sun_current(float);
+    void set_pwm_duty(float);
+    void set_device_temperature(float);
+    void set_capacitor_temperature(float);
+    void set_sun_relay_on(bool);
+    void set_grid_relay_on(bool);
+    void set_transistor_overheat_on(bool);
+    void set_pv_available(bool);
+    void set_current_mode(uint8_t);
+    
+    float get_temperature_max(void);
+    float get_temperature_min(void);
+    float get_temperature_scheduled(void);
+    float get_temperature_moment(void);
+    float get_sun_power_moment(void);
+    float get_sun_voltage(void);
+    float get_sun_current(void);
+    float get_pwm_duty(void);
+    float get_device_temperature(void);
+    float get_capacitor_temperature(void);
+    bool get_sun_relay_on(void);
+    bool get_grid_relay_on(void);
+    bool get_transistor_overheat_on(void);
+    bool get_pv_available(void);
+    uint8_t get_current_mode(void);
 }
 
 #endif
--- src/data.cpp
+++ src/data.cpp
@@ -1,29 +1,85 @@
 #include "mbed.h"
-#include "utils.h"
-#include "pin_names.h"
+#include "data.h"
 
-struct data_collection {
-    float temperature_max;
-    float temperature_min;
-    float temperature_scheduled;
-    float temperature_moment;       //water temperature
-    
-    float moment_sun_watt;
-    float sun_voltage;
-    float sun_current;
-    float pwm_duty;
-    
-    float device_temperature;
-    float capacitor_tempearature;
+extern data_collection nectar_data;
 
-    bool sun_relay_on;
-    bool grid_relay_on;
-    bool transistor_overheat_on;
+namespace data_tests {
+    void set_stats(float temp_moment, float power, float voltage, float current, float duty, float device_temp, float capacitor_temp, bool sun_relay_on, bool grid_relay_on, bool transistor_overheat) {
+        nectar_data.temperature_moment = temp_moment;
+        nectar_data.sun_power_moment = power;
+        nectar_data.sun_voltage = voltage;
+        nectar_data.sun_current = current;
+        nectar_data.pwm_duty = duty;
+        nectar_data.device_temperature = device_temp;
+        nectar_data.capacitor_temperature = capacitor_temp;
+        nectar_data.sun_relay_on = sun_relay_on;
+        nectar_data.grid_relay_on = grid_relay_on;
+        nectar_data.transistor_overheat_on = transistor_overheat;
+    }
     
-    uint8_t current_mode;
-} nectar_data;
-
-namespace data {
+    void set_temperature_max(float temp) {
+        nectar_data.temperature_max = temp;
+    }
+    
+    void set_temperature_min(float temp) {
+        nectar_data.temperature_min = temp;
+    }
+    
+    void set_temperature_scheduled(float temp) {
+        nectar_data.temperature_scheduled = temp;
+    }
+    
+    void set_temperature_moment(float temp) {
+        nectar_data.temperature_moment = temp;
+    }
+    
+    void set_sun_power_moment(float power) {
+        nectar_data.sun_power_moment = power;
+    }
+    
+    void set_sun_voltage(float voltage) {
+        nectar_data.sun_voltage = voltage;
+    }
+    
+    void set_sun_current(float current) {
+        nectar_data.sun_current = current;
+    }
+    
+    void set_pwm_duty(float duty) {
+        nectar_data.pwm_duty = duty;
+    }
+    
+    void set_device_temperature(float temp) {
+        nectar_data.device_temperature = temp;
+    }
+    
+    void set_capacitor_temperature(float temp) {
+        nectar_data.capacitor_temperature = temp;
+    }
+    
+    void set_sun_relay_on(bool state) {
+        nectar_data.sun_relay_on = state;
+    }
+    
+    void set_grid_relay_on(bool state) {
+        nectar_data.grid_relay_on = state;
+    }
+    
+    void set_transistor_overheat_on(bool state) {
+        nectar_data.transistor_overheat_on = state;
+    }
+    
+    void set_pv_available(bool state) {
+        nectar_data.pv_available = state;
+    }
+    
+    void set_current_mode(uint8_t mode) {
+        nectar_data.current_mode = mode;
+    }
+    
+    float get_temperature_moment() {
+        return nectar_data.temperature_moment;
+    }
 }
 
 // *******************************Nectar Sun Copyright © Nectar Sun 2017*************************************   
