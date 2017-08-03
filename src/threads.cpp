e1d9e7b543c3eafebbc04ec18cd3bb6fb1c201ad
paplaukias <domantas.cibas@gmail.com>
2017-08-03 16:37:06 +0300

    moved temperature and device_mode updates to one thread

--- src/threads.cpp
+++ src/threads.cpp
@@ -6,57 +6,31 @@
 Thread thread1;
 Thread thread2;
 Thread thread3;
-Thread thread4;
 
 void service_thread() {
     while(1) comms::service_loop();
 }
 
 void esp_thread() {
-    while(1) comms::esp_loop();
-}
-
-void check_heatsink_temperature() {
     while(1) {
-        //measure T heatsink
-        //if heatsink temp > safe temp ? shutdown power board : run power board
-        Thread::wait(5000);
+        comms::esp_loop();
     }
 }
 
-void check_airgap_temperature() {
-    while(1) {
-        //measure T capacitor
-        //if airgap_temp ? shutdown power board : run power board
-        Thread::wait(5000);
-    }
-}
-
-void update_temperatures() {
+void update_device() {
     while(1) {
         temperature::update();
-        Thread::wait(1000);
-        
-        
-        //if(data.device_temperature > 75.0) {
-        //    power_board::shutdown(1);
-        //}
-    }
-}
-
-void update_device_mode() {
-    while(1) {
+        Thread::wait(500);
         nectar::loop();
-        Thread::wait(1000);
+        Thread::wait(500);
     }
 }
 
 namespace threads {
     uint8_t start(void) {
-        thread1.start(service_thread);
-        //thread2.start(esp_thread);
-        thread3.start(update_temperatures);
-        //thread4.start(update_device_mode);
+        //thread1.start(service_thread);
+        thread2.start(esp_thread);
+        thread3.start(update_device);
     }
 }
 
