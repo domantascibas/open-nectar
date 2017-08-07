2404b84bad051d63c18154ec296d7f716e728be7
paplaukias <domantas.cibas@gmail.com>
2017-08-07 15:56:38 +0300

    parse_fields() is now public function

--- inc/comms.h
+++ inc/comms.h
@@ -5,6 +5,7 @@ namespace comms {
     uint8_t send_cmd_power(uint8_t command);
     uint8_t send_cmd_esp(uint8_t command);
     uint8_t receive_message_power(void);
+    uint8_t parse_fields(char*, char**, uint32_t, char);
 }
 
 #endif
