0782bcac1371cd99206e6de3f271c09e44972ce2
paplaukias <domantas.cibas@gmail.com>
2017-08-07 16:08:06 +0300

    hardware refactor. left only code essential for real-case testing. now has temperature sensor setup and loop. temperature sensor reading is interrupt-driven, added IRQ blocks when writing to data struct.
    add other hardware (temperature sensor, relay, etc) checks in setup

--- src/hardware.cpp
+++ src/hardware.cpp
@@ -1,122 +1,45 @@
 #include "mbed.h"
 #include "hardware.h"
-#include "DS1820.h"
 #include "data.h"
-#include "comms.h"
-#include "power_board.h"
-#include "service.h"
-#include "esp.h"
+#include "DS1820.h"
 
-//user interface
-//OLED SDA PB_7, SCL PB_6
-//DigitalIn left PA_5
-//DigitalIn center PA_6
-//DigitalIn right PA_7
-
-//temperature sensors
+Ticker update_temperature;
 DS1820  probe_boiler(PB_8);
 DS1820  probe_internal(PB_9);
 
-//relays
-DigitalOut relay_sun(PB_3);
-DigitalOut relay_grid(PB_4);
-
-//other peripherals
-DigitalIn zero_cross(PB_5);
-DigitalOut led1(LED1);
-
-extern RawSerial comms_pc;
 extern Data data;
 
 namespace hardware {
-    uint8_t response;
+    volatile bool update_temp = false;
     
-    //setup all hardware when started
-    uint8_t setup(void) {
-        service::init(115200);
-        power_board::init(19200);
-        esp::init(19200);
-        comms_pc.printf("[COMMS] Started\r\n");
-        
-        response = temperature::setup();
-        if(response != NS_OK) {
-            comms_pc.printf("[TEMPERATURE] Setup failed!\r\n");
-        } else {
-            comms_pc.printf("[TEMPERATURE] INT and AUX started\r\n");
-        }
-        
-        return NS_OK;
+    void update() {
+        update_temp = true;
     }
-    
-    uint8_t startup(void) {
-        uint8_t resp;
-        //startup sequence
-        //run safety checks, instruct power board to turn on/off
         
-        //init boiler thermometer
-        //check if >0;
-        data.temp_boiler = probe_boiler.read();
-        probe_boiler.startConversion();
-        if(data.temp_boiler < 1) {
-            printf("[BOILER] ERROR\r\n");
-        }
-        
-        wait(1.0);
-        comms_pc.printf("\nPOWER BOARD Starting\r\n");
-        resp = power_board::start();
-        if(resp == 0x06) {
-            relay_sun = true;
-            comms_pc.printf("Started\r\n");
-        } else {
-            relay_sun = false;
-            //resp = comms_pc.getc();
-            comms_pc.printf("Not Started: 0x%X\r\n", resp);
-        }
-        data.sun_relay_on = relay_sun;
-
-        
-        //init heatsink thermometer
-        //check if <40C
-        
-        //init capacitor thermometer
-        //check if <40C
-        
-        //init transistor overheat
-        //check if off
-        
-        //comms_power.putc(POWER_BOARD_START);
-        //wait for response from power board
-        
-        //check sun relay
-        //turn on sun relay, measure current
-        
-        //check grid relay
-        //turn on grid relay, measure boiler temperature
-        
-        return NS_OK;
-    }
-}
-
-namespace temperature {
-    uint8_t setup(void) {
+    uint8_t setup() {
+        //setup temperature sensors
         probe_boiler.begin();
         probe_boiler.startConversion();
         probe_internal.begin();
         probe_internal.startConversion();
         wait(0.5);
+        //setup hardware, run self checks
         
-        //TODO add check to see if thermometers are connected        
-        return NS_OK;
+        update_temperature.attach(&update, 5.0);
     }
     
-    uint8_t update(void) {
-        data.temp_boiler = probe_boiler.read();
-        data.device_temperature = probe_internal.read();
-        //printf("[TEMPERATURE] boiler %.2f\r\n", data.temp_boiler);
-        //printf("[TEMPERATURE] device %.2f\r\n", data.device_temperature);
-        
-        probe_boiler.startConversion();
-        probe_internal.startConversion();
+    void loop() {
+        if(update_temp) {
+            update_temp = false;
+            __disable_irq();
+            data.temp_boiler = probe_boiler.read();
+            data.device_temperature = probe_internal.read();
+            __enable_irq();
+            
+            probe_boiler.startConversion();
+            probe_internal.startConversion();
+            printf("[TEMPERATURE] boiler %.2f\r\n", data.temp_boiler);
+        }
     }
 }
 
