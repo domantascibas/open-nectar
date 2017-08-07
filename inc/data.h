8ebfd7a4ce0b0d4cf2fb4f1b5a8b014d35024773
paplaukias <domantas.cibas@gmail.com>
2017-08-07 15:57:39 +0300

    removed unused commands

--- inc/data.h
+++ inc/data.h
@@ -12,22 +12,6 @@
 #define AUTO_MODE                   0x61    //'a'
 #define MANUAL_MODE                 0x6D    //'m'
 
-#define CMD_POWER_BOARD_START       0x0F
-#define CMD_POWER_BOARD_STOP        0xF0
-
-#define CMD_GET_VOLTAGE             0x56    //'V'
-#define CMD_GET_CURRENT             0x49    //'I'
-#define CMD_GET_REF_VOLTAGE         0x4B    //'K'
-#define CMD_GET_REF_CURRENT         0x4C    //'L'
-
-#define CMD_SET_PWM_DUTY            0x44    //'D'
-#define CMD_GET_PWM_DUTY            0x45    //'E'
-#define CMD_GET_CAP_TEMP            0x4E    //'P'
-#define CMD_GET_MOSFET_OHEAT        0x4F    //'O'
-
-#define CMD_PWM_ON                  0x41    //'A'
-#define CMD_PWM_OFF                 0x42    //'B'
-
 #define SET_MODE                    0x4D    //'M'
 #define GET_STATS                   0x54    //'T'
 
