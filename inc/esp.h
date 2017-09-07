3bd45b34f26067a6df331071e1c89cbfe3b90ee7
paplaukias <domantas.cibas@gmail.com>
2017-09-07 16:46:59 +0300

    implemented nectar_serial protocol

--- inc/esp.h
+++ inc/esp.h
@@ -1,10 +1,36 @@
 #ifndef ESP_H
 #define ESP_H
 
+#include "NectarStream.h"
+
 namespace esp {
+  extern nectar_contract::MainBoardStats stats;
+  extern nectar_contract::NectarStatus status;
+  
   void setup();
   void loop();
 }
+
+class mbedStream : public IStreamDelegate {
+public:
+  mbedStream(PinName transmitPin, PinName receivePin, bool inverse_logic = false,
+                unsigned int buffSize = 64)
+      : m_serial(transmitPin, receivePin),
+        stream(*this) {}
+
+  ~mbedStream(){};
+
+  void setup();
+  void Rx_interrupt();
+
+  virtual void write(uint8_t byte);
+  virtual void received_main_board_stats(const nectar_contract::MainBoardStats &stats);
+  virtual void received_esp_status(const nectar_contract::NectarStatus &status);
+  NectarStream stream;
+
+private:
+  RawSerial m_serial;
+};
 
 #endif
 
--- src/esp.cpp
+++ src/esp.cpp
@@ -1,6 +1,8 @@
 #include "mbed.h"
 #include "data.h"
 #include "esp.h"
+#include "NectarStream.h"
+#include "NectarContract.h"
 
 static const PinName TX = PA_9;
 static const PinName RX = PA_10;
@@ -8,50 +10,48 @@ static const PinName RX = PA_10;
 static const uint8_t SET_MODE = 0x4D;       //'M'
 static const uint8_t GET_STATS = 0x54;      //'T'
 
-RawSerial esp_serial(TX, RX);
+mbedStream m_stream(TX, RX);
 
 namespace esp {
   static volatile char command = NULL;
-
-  void Rx_interrupt() {
-    command = esp_serial.getc();
-  }
-
-  //can send 64 chars max
-  void send_stats() {
-    __disable_irq();
-    esp_serial.printf("#%.2f,%d,%.2f,%d,%.2f,%.2f,%.2f,%d,%.2f,%.2f,%.2f$\n",
-      data.pv_power, data.grid_relay_on, data.temp_boiler, data.sun_relay_on, data.pv_voltage, data.pv_current,
-      data.device_temperature, data.mosfet_overheat_on, data.radiator_temp, data.pwm_duty, data.airgap_temp
-    );
-    __enable_irq();
+  
+  void send_stats() {    
+    nectar_contract::MainBoardStats stats = {
+      data.pv_power, data.grid_relay_on, data.temp_boiler, data.sun_relay_on,
+      data.pv_voltage, data.pv_current, data.device_temperature, data.mosfet_overheat_on,
+      data.radiator_temp, data.pwm_duty, data.airgap_temp, data.error, 0
+    };
     
-    printf("V:%5.2f I:%5.2f D:%5.2f Tmosfet:%5.2f Overheat:%d Tairgap:%5.2f\r\n",
-    data.pv_voltage, data.pv_current, data.pwm_duty, data.radiator_temp, data.mosfet_overheat_on, data.airgap_temp);
+    m_stream.stream.sendObject(C_MAIN_BOARD_STATS, &stats, sizeof(stats));
   }
 
   void setup() {
-    static const uint16_t baudrate = 19200;
-    
-    esp_serial.baud(baudrate);
-    esp_serial.attach(&Rx_interrupt);
+    m_stream.setup();
   }
 
   void loop() {
-    if(command != NULL) {
-      printf("cmd from esp: 0x%X\r\n", command);
-      switch(command) {
-        case GET_STATS:
-          send_stats();
-        break;
-
-        default:
-          //do nothing
-        break;
-      }
-      command = NULL;
-    }
   }
 }
+
+/*
+ * Class implementation
+ */
+
+void mbedStream::setup() {
+  m_serial.baud(19200);
+  m_serial.attach(this, &mbedStream::Rx_interrupt);
+}
+
+void mbedStream::Rx_interrupt() {
+  stream.receiveChar(m_serial.getc());
+}
+
+void mbedStream::write(uint8_t byte) { m_serial.putc(byte); }
+
+void mbedStream::received_main_board_stats(const nectar_contract::MainBoardStats &stats) {
+  esp::send_stats();
+}
+
+void mbedStream::received_esp_status(const nectar_contract::NectarStatus &status) {}
 
 // *******************************Nectar Sun Copyright © Nectar Sun 2017*************************************   
