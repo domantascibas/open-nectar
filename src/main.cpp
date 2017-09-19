f92260f072d031ba355c6271b697e3e284440f79
paplaukias <domantas.cibas@gmail.com>
2017-09-19 16:42:48 +0300

    prints RAM available. working menu

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,36 @@
 #include "service.h"
 #include "menu_service.h"
 
+// Function prototype
+ 
+int availableMemory(int = 256, int = 0x8000, bool = true);
+ 
+// Calculation function 
+ 
+int availableMemory(int resolution, int maximum, bool disableInterrupts) {
+    if (resolution < 1) resolution = 1;
+    if (maximum < 0) maximum = 0;
+ 
+    int low = 0;
+    int high = maximum + 1;
+ 
+    if (disableInterrupts) __disable_irq();
+    while (high - low > resolution) {
+        int mid = (low + high) / 2;
+        void* p = malloc(mid);
+        if (p == NULL) {
+            high = mid;
+        } else {
+            free(p);
+            low = mid;
+        }
+    }
+    if (disableInterrupts) __enable_irq();
+    return low;
+}
+ 
+// Calling the function
+ 
 int main() {
   menu_service::setup();
   service::setup();
@@ -13,10 +43,13 @@ int main() {
   wait(2.0);
 //  power_board::setup();
   esp::setup();
+  
+  menu_service::updated = true;
 
   while(1) {
     if(menu_service::updated) {
       menu_service::updateScreen();
+      printf("\nAvailable memory = %d\r\n", availableMemory(1) );
     }
     service::loop();
     hardware::updateTemperature();
