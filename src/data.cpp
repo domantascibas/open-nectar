f5eadb6886a505cbd2d21b42b70598e9d8d030fa
paplaukias <domantas.cibas@gmail.com>
2017-10-02 17:12:39 +0300

    adds init values to the data struct

--- src/data.cpp
+++ src/data.cpp
@@ -1,6 +1,31 @@
 #include "mbed.h"
 #include "data.h"
 
-Data data = { 0, 0.00, 0.00, 0.00, 0.00, 0.00, 0.1, 0.0, 0.0, false, false, false, 0x00 };
+uint8_t current_state = IDLE;
+  
+float moment_power = 0.00;
+float moment_voltage = 0.00;
+float moment_current = 0.00;
+
+float reference_voltage = 0.00;
+float reference_current = 0.00;
+  
+float grid_energy_meter_kwh = 0.00;
+float sun_energy_meter_kwh = 0.00;
+
+float pwm_duty = 0.1;
+float airgap_temperature = 0.00;
+float radiator_temperature = 0.00;
+
+bool mosfet_overheat_on = false;
+bool calibrated = false;
+bool generator_on = false;
+  
+uint8_t power_board_error = 0x00;
+  
+Data data = {
+  current_state, moment_power, moment_voltage, moment_current, reference_voltage, reference_current, grid_energy_meter_kwh, sun_energy_meter_kwh,
+  pwm_duty, airgap_temperature, radiator_temperature, mosfet_overheat_on, calibrated, generator_on, power_board_error
+};
 
 // *******************************Nectar Sun Copyright © Nectar Sun 2017*************************************   
