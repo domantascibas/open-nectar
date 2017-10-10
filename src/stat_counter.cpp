ffdddd2251bd30ae6363f85ae7cd281a15bd2ebf
paplaukias <domantas.cibas@gmail.com>
2017-10-10 14:21:40 +0300

    moves boiler_power to data struct

--- src/stat_counter.cpp
+++ src/stat_counter.cpp
@@ -5,7 +5,6 @@
 //TODO move boiler power to the Data struct
 
 Timer stat_timer;
-static const float boiler_power = 2.0;
 
 namespace stat_counter {
   void setup() {
@@ -18,7 +17,7 @@ namespace stat_counter {
     } else {
       float time_passed = stat_timer.read();
       stat_timer.reset();
-      data.grid_kwh += boiler_power * time_passed / 3600 / 1000;
+      data.grid_kwh += data.boiler_power * time_passed / 3600 / 1000;
     }
     printf("solar: %.2fkWh grid: %.2fkWh\r\n", data.solar_kwh, data.grid_kwh);
   }
