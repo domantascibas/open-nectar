8a375fef329f06b6bdd91f307cddc3256ccc2b77
paplaukias <domantas.cibas@gmail.com>
2017-10-11 16:38:36 +0300

    moves free memory calculations to a separate file

---
+++ inc/memory_checker.h
@@ -0,0 +1,8 @@
+#ifndef MEMORY_CHECKER_H
+#define MEMORY_CHECKER_H
+
+namespace Memory {
+  int availableMemory(int = 256, int = 0x8000, bool = true);
+}
+
+#endif
---
+++ memory_checker.cpp
@@ -0,0 +1,27 @@
+#include "stdint.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+namespace Memory {
+  int availableMemory(int resolution, int maximum, bool disableInterrupts) {
+    if(resolution < 1) resolution = 1;
+    if(maximum < 0) maximum = 0;
+
+    int low = 0;
+    int high = maximum + 1;
+
+    if(disableInterrupts) __disable_irq();
+    while(high - low > resolution) {
+      int mid = (low + high) / 2;
+      void* p = malloc(mid);
+      if(p == NULL) {
+        high = mid;
+      } else {
+        free(p);
+        low = mid;
+      }
+    }
+    if(disableInterrupts) __enable_irq();
+    return low;
+  }
+}
