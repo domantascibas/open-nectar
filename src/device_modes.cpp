1898f46686f61ae08600bb9de87c377fc61bf9ce
Domantas Cibas <domantas@nectarsun.com>
2017-10-29 22:35:24 +0200

    replaces old relay switching implementation with new OOP-based implementation

--- src/device_modes.cpp
+++ src/device_modes.cpp
@@ -1,96 +1,27 @@
-#include "mbed.h"
 #include "device_modes.h"
-#include "power_board.h"
 #include "stat_counter.h"
 #include "OperationalMode.h"
+#include "RelayController.h"
 #include "data.h"
 #include "esp.h"
 
 static const float BOOST_TEMP = 70.0;
-
-static const PinName SUN = PB_3;
-static const PinName GRID = PB_4;
-static const PinName ZERO_CROSS = PB_5;
 static const float HIST = 2.0;
 
 Ticker update_mode_tick;
-Timer delay_t;
-DigitalOut relay_sun(SUN);
-DigitalOut relay_grid(GRID);
-DigitalIn zero_cross(ZERO_CROSS);
+RelayController relayController;
 
 namespace device_modes {
   volatile bool update_mode = false;
-  
-  enum power_source {
-    TURN_ON_GRID,
-    TURN_ON_SUN,
-    TURN_OFF_ALL
-  };
-  
-  void delay(float seconds) {
-    delay_t.reset();
-    while(delay_t.read() < seconds) {}
-  }
-  
-  void grid_relay_state(bool state) {
-    delay_t.reset();
-    while(!zero_cross) {}
-    while(delay_t.read_us() < 600) {}
-    relay_grid = state;
-  }
   
   void update_mode_ISR() {
     update_mode = true;
   }
   
-  uint8_t response = TURN_OFF_ALL;
-
-  void set_relays() {
-    switch(data.relay_state) {
-      default:
-      case TURN_OFF_ALL:
-        power_board::stop();
-        //while(data.pv_current > 0.1) {}
-        delay(1.0);
-        relay_sun = false;
-        grid_relay_state(false);
-        printf("turn off all\r\n");
-      break;
-
-      case TURN_ON_GRID:
-        power_board::stop();
-        delay(1.0);
-        //while(data.pv_current > 0.1) {}
-        relay_sun = false;
-        delay(1.0);
-        grid_relay_state(true);
-        printf("turn on grid\r\n");
-      break;
-
-      case TURN_ON_SUN:
-        grid_relay_state(false);
-        if((!data.generator_on) && (data.power_board_error == 0)) {
-          delay(1.0);
-          relay_sun = true;
-          delay(1.0);
-          power_board::start();
-          printf("power board start\r\n");
-        }
-        printf("turn on sun\r\n");
-      break;
-    }
-//      __disable_irq();
-    data.grid_relay_on = relay_grid;
-    data.sun_relay_on = relay_sun;
-//      __enable_irq();
-      
-  }
-  
   void setup() {
     update_mode_tick.attach(update_mode_ISR, 1.0);
     stat_counter::setup();
-    delay_t.start();
+    relayController.turnOffAll();
   }
 
   void loop() {
@@ -114,23 +45,23 @@ namespace device_modes {
           //printf("T_max = %5.2fC T_curr = %5.2fC T_set = %5.2fC PV = %d ", nectar_data.temperature_max, nectar_data.temperature_moment, nectar_data.temperature_scheduled, nectar_data.pv_available);
           if((temp_boiler < temp_min) || (temp_boiler > temp_max)) {
             printf("temp %f  > max temp %f\r\n", temp_boiler, temp_max);
-            response = TURN_OFF_ALL;
+            relayController.turnOffAll();
             //data.error = BOILER_TEMP_SENSOR_ERROR;
           } else {
-            response = TURN_ON_SUN;
-            if(relay_grid) {
+            if(relayController.isGridRelayOn()) {
               if(temp_boiler < (temp + HIST)) {
-                response = TURN_ON_GRID;
+                relayController.turnOnGrid();
               } else {
-                response = TURN_ON_SUN;
+                relayController.turnOnSun();
               }
-            }
-            if(relay_sun) {
+            } else if(relayController.isSunRelayOn()) {
               if(temp_boiler > (temp - HIST)) {
-                response = TURN_ON_SUN;
+                relayController.turnOnSun();
               } else {
-                response = TURN_ON_GRID;
+                relayController.turnOnGrid();
               }
+            } else {
+              relayController.turnOnSun();
             }
           }
         break;
@@ -140,7 +71,7 @@ namespace device_modes {
           //TODO boost mode heats water up to 66C
           printf("[MODE] BOOST\r\n");
           if((temp_boiler < temp_min) || (temp_boiler > temp_max)) {
-            response = TURN_OFF_ALL;
+            relayController.turnOffAll();
             //data.error = BOILER_TEMP_SENSOR_ERROR;
             if(temp_boiler > BOOST_TEMP) {
               printf("Boost finished. Boiler temp %f  > max temp %f\r\n", temp_boiler, BOOST_TEMP);
@@ -154,7 +85,7 @@ namespace device_modes {
               }
             }
           } else {
-            response = TURN_ON_GRID;
+            relayController.turnOnGrid();
           }
         break;
 
@@ -162,23 +93,23 @@ namespace device_modes {
           printf("[MODE] AWAY\r\n");
           //TODO add temp_max 45.0
           if((temp_boiler < temp_min) || (temp_boiler > temp_max)) {
-            response = TURN_OFF_ALL;
+            relayController.turnOffAll();
             //data.error = BOILER_TEMP_SENSOR_ERROR;
           } else {
-            response = TURN_ON_SUN;
-            if(relay_grid) {
+            if(relayController.isGridRelayOn()) {
               if(temp_boiler < (temp_away + HIST)) {
-                response = TURN_ON_GRID;
+                relayController.turnOnGrid();
               } else {
-                response = TURN_ON_SUN;
+                relayController.turnOnSun();
               }
-            }
-            if(relay_sun) {
+            } else if(relayController.isSunRelayOn()) {
               if(temp_boiler > (temp_away - HIST)) {
-                response = TURN_ON_SUN;
+                relayController.turnOnSun();
               } else {
-                response = TURN_ON_GRID;
+                relayController.turnOnGrid();
               }
+            } else {
+              relayController.turnOnSun();
             }
           }
         break;
@@ -187,16 +118,12 @@ namespace device_modes {
           printf("[MODE] NO GRID\r\n");
           //TODO turn on grid when temperature drops below 5.0C
           if((temp_boiler < temp_min) || (temp_boiler > temp_max)) {
-            response = TURN_OFF_ALL;
+            relayController.turnOffAll();
             //data.error = BOILER_TEMP_SENSOR_ERROR;
           } else {
-            response = TURN_ON_SUN;
+            relayController.turnOnSun();
           }
         break;
-      }
-      if(response != data.relay_state) {
-        data.relay_state = response;
-        set_relays();
       }
     }
   }
