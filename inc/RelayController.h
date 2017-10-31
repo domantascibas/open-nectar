62b16e3046c397d9c765beba8f67394c4a356c79
Domantas Cibas <domantas@nectarsun.com>
2017-10-31 11:15:12 +0200

    updates RelayController

--- inc/RelayController.h
+++ inc/RelayController.h
@@ -4,15 +4,23 @@
 #include "GridRelay.h"
 #include "SunRelay.h"
 
+enum RelayStates {
+  TURN_OFF_ALL,
+  TURN_ON_SUN,
+  TURN_ON_GRID
+};
+
 struct RelayController {
   RelayController();
   
-  void turnOnGrid();
-  void turnOnSun();
-  void turnOffAll();
-  
+  void init();
+  void setRelays(uint8_t);
+  uint8_t getRelayState();
   bool isSunRelayOn();
   bool isGridRelayOn();
+  
+  private:
+  uint8_t relayState;
 };
 
 #endif
--- src/RelayController.cpp
+++ src/RelayController.cpp
@@ -6,36 +6,13 @@ GridRelay gridRelay;
 SunRelay sunRelay;
 
 RelayController::RelayController() {
+  relayState = TURN_OFF_ALL;
+}
+
+void RelayController::init() {
   gridRelay.init();
   sunRelay.init();
-}
-
-void RelayController::turnOffAll() {
-  sunRelay.turnOff();
-  gridRelay.turnOff();
-  data.grid_relay_on = gridRelay.isOn();
-  data.sun_relay_on = sunRelay.isOn();
-  printf("turn off all\r\n");
-}
-
-void RelayController::turnOnGrid() {
-  sunRelay.turnOff();
-  gridRelay.turnOn();
-  data.grid_relay_on = gridRelay.isOn();
-  data.sun_relay_on = sunRelay.isOn();
-  printf("turn on grid\r\n");
-}
-
-void RelayController::turnOnSun() {
-  if(!powerBoardError.has_errors) {
-    gridRelay.turnOff();
-    sunRelay.turnOn();
-    data.grid_relay_on = gridRelay.isOn();
-    data.sun_relay_on = sunRelay.isOn();
-    printf("turn on sun\r\n");
-  } else {
-    turnOffAll();
-  }
+  setRelays(TURN_OFF_ALL);
 }
 
 bool RelayController::isGridRelayOn() {
@@ -44,4 +21,47 @@ bool RelayController::isGridRelayOn() {
 
 bool RelayController::isSunRelayOn() {
   return sunRelay.isOn();
+}
+
+uint8_t RelayController::getRelayState() {
+  return relayState;
+}
+
+void RelayController::setRelays(uint8_t state) {
+  relayState = state;
+  if(mainBoardError.has_error(DEVICE_OVERHEAT) || mainBoardError.has_error(MIN_TEMPERATURE) || mainBoardError.has_error(MAX_TEMPERATURE)) {
+    relayState = TURN_OFF_ALL;
+    printf("overheat/temperature-related errors\r\n");
+  }
+  switch(relayState) {
+    default:
+    case TURN_OFF_ALL:
+      sunRelay.turnOff();
+      gridRelay.turnOff();
+      data.grid_relay_on = gridRelay.isOn();
+      data.sun_relay_on = sunRelay.isOn();
+      printf("turn off all\r\n");
+      break;
+    
+    case TURN_ON_SUN:
+      if(!powerBoardError.has_errors) {
+        gridRelay.turnOff();
+        sunRelay.turnOn();
+        data.grid_relay_on = gridRelay.isOn();
+        data.sun_relay_on = sunRelay.isOn();
+        printf("turn on sun\r\n");
+      } else {
+        printf("power board error.\r\n");
+        setRelays(TURN_OFF_ALL);
+      }
+      break;
+    
+    case TURN_ON_GRID:
+      sunRelay.turnOff();
+      gridRelay.turnOn();
+      data.grid_relay_on = gridRelay.isOn();
+      data.sun_relay_on = sunRelay.isOn();
+      printf("turn on grid\r\n");
+      break;
+  }
 }
