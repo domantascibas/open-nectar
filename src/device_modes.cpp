0c247dc22792eb09600b147de79eca01c2e993ba
Domantas Cibas <domantas@nectarsun.com>
2017-11-07 18:05:36 +0200

    adds a check and 5C drop when max temperature is reached

--- src/device_modes.cpp
+++ src/device_modes.cpp
@@ -10,6 +10,7 @@ Ticker update_mode_tick;
 RelayController relayController;
 
 namespace device_modes {
+  bool reachedMaxTemp = false;
   bool updateHeaterMode = false;
   static bool isFirst = true;
   uint8_t relayStateNew = TURN_OFF_ALL;
@@ -50,95 +51,109 @@ namespace device_modes {
       float temp_min = temperatureData.getMinTemperature();
       float temp_max = temperatureData.getMaxTemperature();
       
-      switch(DataService::getCurrentHeaterMode()) {
-        default:  //mode is any other value than the ones below
-        case nectar_contract::None:
-          printf("[MODE] DEFAULT\r\n");
-          //printf("T_max = %5.2fC T_curr = %5.2fC T_set = %5.2fC PV = %d ", nectar_data.temperature_max, nectar_data.temperature_moment, nectar_data.temperature_scheduled, nectar_data.pv_available);
-          if((temp_boiler < temp_min) || (temp_boiler > temp_max)) {
-            printf("temp %f  > max temp %f\r\n", temp_boiler, temp_max);
-            relayStateNew = TURN_OFF_ALL;
-            //data.error = BOILER_TEMP_SENSOR_ERROR;
-          } else if(isFirst) {
-            isFirst = false;
-            if(temp_boiler < temp) {
-              relayStateNew = TURN_ON_GRID;
-            } else {
-              relayStateNew = TURN_ON_SUN;
-            }
-          } else {
-            if(relayController.isGridRelayOn()) {
-              if(temp_boiler < (temp + HIST)) {
-                relayStateNew = TURN_ON_GRID;
-              } else {
-                relayStateNew = TURN_ON_SUN;
+      if(reachedMaxTemp && ((temp_boiler - 5.0) < temp_max)) reachedMaxTemp = false;
+      if(reachedMaxTemp) {
+        relayStateNew = TURN_OFF_ALL;
+      } else {
+        switch(DataService::getCurrentHeaterMode()) {
+          default:
+          case nectar_contract::None:
+            printf("[MODE] DEFAULT\r\n");
+            if((temp_boiler < temp_min) || (temp_boiler > temp_max)) {
+              if(temp_boiler > temp_max) {
+                printf("temp %f  > max temp %f\r\n", temp_boiler, temp_max);
+                reachedMaxTemp = true;
               }
-            } else if(relayController.isSunRelayOn()) {
-              if(temp_boiler > (temp - HIST)) {
-                relayStateNew = TURN_ON_SUN;
-              } else {
+              relayStateNew = TURN_OFF_ALL;
+              //data.error = BOILER_TEMP_SENSOR_ERROR;
+            } else if(isFirst) {
+              isFirst = false;
+              if(temp_boiler < temp) {
                 relayStateNew = TURN_ON_GRID;
+              } else {
+                relayStateNew = TURN_ON_SUN;
               }
             } else {
-              relayStateNew = TURN_ON_SUN;
-            }
-          }
-        break;
-
-        case nectar_contract::Boost:
-          //TODO after boost completes, return to previous mode
-          //TODO boost mode heats water up to 66C
-          printf("[MODE] BOOST\r\n");
-          if((temp_boiler < temp_min) || (temp_boiler > temp_max)) {
-            relayStateNew = TURN_OFF_ALL;
-            //data.error = BOILER_TEMP_SENSOR_ERROR;
-            if(temp_boiler > BOOST_TEMP) {
-              printf("Boost finished. Boiler temp %f  > max temp %f\r\n", temp_boiler, BOOST_TEMP);
-              DataService::setPreviousHeaterMode();
-            }
-          } else {
-            relayStateNew = TURN_ON_GRID;
-          }
-        break;
-
-        case nectar_contract::Away:
-          printf("[MODE] AWAY\r\n");
-          //TODO add temp_max 45.0
-          if((temp_boiler < temp_min) || (temp_boiler > temp_max)) {
-            relayStateNew = TURN_OFF_ALL;
-            //data.error = BOILER_TEMP_SENSOR_ERROR;
-          } else {
-            if(relayController.isGridRelayOn()) {
-              if(temp_boiler < (AWAY_TEMP + HIST)) {
-                relayStateNew = TURN_ON_GRID;
+              if(relayController.isGridRelayOn()) {
+                if(temp_boiler < (temp + HIST)) {
+                  relayStateNew = TURN_ON_GRID;
+                } else {
+                  relayStateNew = TURN_ON_SUN;
+                }
+              } else if(relayController.isSunRelayOn()) {
+                if(temp_boiler > (temp - HIST)) {
+                  relayStateNew = TURN_ON_SUN;
+                } else {
+                  relayStateNew = TURN_ON_GRID;
+                }
               } else {
                 relayStateNew = TURN_ON_SUN;
               }
-            } else if(relayController.isSunRelayOn()) {
-              if(temp_boiler > (AWAY_TEMP - HIST)) {
-                relayStateNew = TURN_ON_SUN;
+            }
+          break;
+
+          case nectar_contract::Boost:
+            printf("[MODE] BOOST\r\n");
+            if((temp_boiler < temp_min) || (temp_boiler > temp_max)) {
+              if(temp_boiler > temp_max) {
+                printf("temp %f  > max temp %f\r\n", temp_boiler, temp_max);
+                reachedMaxTemp = true;
+              }
+              relayStateNew = TURN_OFF_ALL;
+              //data.error = BOILER_TEMP_SENSOR_ERROR;
+            } else {
+              if(temp_boiler > BOOST_TEMP) {
+                printf("Boost finished. Boiler temp %f  > max temp %f\r\n", temp_boiler, BOOST_TEMP);
+                DataService::setPreviousHeaterMode();
               } else {
                 relayStateNew = TURN_ON_GRID;
               }
+            }
+          break;
+
+          case nectar_contract::Away:
+            printf("[MODE] AWAY\r\n");
+            if((temp_boiler < temp_min) || (temp_boiler > temp_max)) {
+              if(temp_boiler > temp_max) {
+                printf("temp %f  > max temp %f\r\n", temp_boiler, temp_max);
+                reachedMaxTemp = true;
+              }
+              relayStateNew = TURN_OFF_ALL;
+              //data.error = BOILER_TEMP_SENSOR_ERROR;
+            } else {
+              if(relayController.isGridRelayOn()) {
+                if(temp_boiler < (AWAY_TEMP + HIST)) {
+                  relayStateNew = TURN_ON_GRID;
+                } else {
+                  relayStateNew = TURN_ON_SUN;
+                }
+              } else if(relayController.isSunRelayOn()) {
+                if(temp_boiler > (AWAY_TEMP - HIST)) {
+                  relayStateNew = TURN_ON_SUN;
+                } else {
+                  relayStateNew = TURN_ON_GRID;
+                }
+              } else {
+                relayStateNew = TURN_ON_SUN;
+              }
+            }
+          break;
+
+          case nectar_contract::Nogrid:
+            printf("[MODE] NO GRID\r\n");
+            if((temp_boiler < temp_min) || (temp_boiler > temp_max)) {
+              if(temp_boiler > temp_max) {
+                printf("temp %f  > max temp %f\r\n", temp_boiler, temp_max);
+                reachedMaxTemp = true;
+              }
+              relayStateNew = TURN_OFF_ALL;
+              //data.error = BOILER_TEMP_SENSOR_ERROR;
             } else {
               relayStateNew = TURN_ON_SUN;
+              printf("[NO GRID] TURN ON SUN\r\n");
             }
-          }
-        break;
-
-        case nectar_contract::Nogrid:
-          printf("[MODE] NO GRID\r\n");
-          //TODO turn on grid when temperature drops below 5.0C
-          if((temp_boiler < temp_min) || (temp_boiler > temp_max)) {
-            relayStateNew = TURN_OFF_ALL;
-            printf("[NO GRID] TURN OFF ALL\r\n");
-            printf("[NO GRID] min:%f < %f < max:%f\r\n", temp_min, temp_boiler, temp_max);
-            //data.error = BOILER_TEMP_SENSOR_ERROR;
-          } else {
-            relayStateNew = TURN_ON_SUN;
-            printf("[NO GRID] TURN ON SUN\r\n");
-          }
-        break;
+          break;
+        }
       }
       
       if(relayStateNew != relayController.getRelayState()) {
