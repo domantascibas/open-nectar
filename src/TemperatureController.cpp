44fb7ea423bdd4de403a953bff34035c3e38124f
Domantas Cibas <domantas@nectarsun.com>
2017-11-07 17:59:33 +0200

    updates the updateHeaterMode flag when new boiler temp is available

--- src/TemperatureController.cpp
+++ src/TemperatureController.cpp
@@ -1,6 +1,7 @@
 #include "TemperatureController.h"
 #include "ErrorHandler.h"
 #include "DataService.h"
+#include "device_modes.h"
 
 static const PinName BOILER_TEMP_PROBE = PB_8;
 static const PinName DEVICE_TEMP_PROBE = PB_9;
@@ -26,35 +27,34 @@ void TemperatureController::init() {
 }
 
 float TemperatureController::getBoilerTemperature() {
-  if(boilerTemp.isNewValueAvailable()) {
-    boilerTemperature = boilerTemp.getTemperature();
-    if(boilerTemperature > WATER_TEMPERATURE_LIMIT_MAX) {
-      mainBoardError.set_error(MAX_TEMPERATURE);
-    } else if(boilerTemperature < WATER_TEMPERATURE_LIMIT_MIN) {
-      mainBoardError.set_error(MIN_TEMPERATURE);
-    } else {
-      if(mainBoardError.has_error(MAX_TEMPERATURE)) mainBoardError.clear_error(MAX_TEMPERATURE);
-      if(mainBoardError.has_error(MIN_TEMPERATURE)) mainBoardError.clear_error(MIN_TEMPERATURE);
-      printf("[TEMPERATURE] boiler %.2f\r\n", boilerTemperature);
-    }
+  boilerTemperature = boilerTemp.getTemperature();
+  if(boilerTemperature > WATER_TEMPERATURE_LIMIT_MAX) {
+    mainBoardError.set_error(MAX_TEMPERATURE);
+  } else if(boilerTemperature < WATER_TEMPERATURE_LIMIT_MIN) {
+    mainBoardError.set_error(MIN_TEMPERATURE);
+  } else {
+    if(mainBoardError.has_error(MAX_TEMPERATURE)) mainBoardError.clear_error(MAX_TEMPERATURE);
+    if(mainBoardError.has_error(MIN_TEMPERATURE)) mainBoardError.clear_error(MIN_TEMPERATURE);
+    printf("[TEMPERATURE] boiler %.2f\r\n", boilerTemperature);
   }
   return boilerTemperature;
 }
 
 float TemperatureController::getDeviceTemperature() {
-  if(deviceTemp.isNewValueAvailable()) {
-    deviceTemperature = deviceTemp.getTemperature();
-    if(deviceTemperature > DEVICE_TEMPERATURE_LIMIT_MAX) {
-      mainBoardError.set_error(DEVICE_OVERHEAT);
-    } else {
-      if(mainBoardError.has_error(DEVICE_OVERHEAT)) mainBoardError.clear_error(DEVICE_OVERHEAT);
-      printf("[TEMPERATURE] internal %.2f\r\n", deviceTemperature);
-    }
+  deviceTemperature = deviceTemp.getTemperature();
+  if(deviceTemperature > DEVICE_TEMPERATURE_LIMIT_MAX) {
+    mainBoardError.set_error(DEVICE_OVERHEAT);
+  } else {
+    if(mainBoardError.has_error(DEVICE_OVERHEAT)) mainBoardError.clear_error(DEVICE_OVERHEAT);
+    printf("[TEMPERATURE] internal %.2f\r\n", deviceTemperature);
   }
   return deviceTemperature;
 }
 
 void TemperatureController::updateTemperatures() {
-  temperatureData.setBoilerTemperature(getBoilerTemperature());
-  temperatureData.setDeviceTemperature(getDeviceTemperature());
+  if(deviceTemp.isNewValueAvailable()) temperatureData.setDeviceTemperature(getDeviceTemperature());
+  if(boilerTemp.isNewValueAvailable()) {
+    temperatureData.setBoilerTemperature(getBoilerTemperature());
+    device_modes::updateHeaterMode = true;
+  }
 }
