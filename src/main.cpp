0a2c90d47f4c8da794975e9de65357260eddc14b
paplaukias <domantas.cibas@gmail.com>
2017-11-20 13:19:49 +0200

    updates versioning

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,9 @@
 #include "service.h"
 #include "ErrorHandler.h"
 
-#define VERSION 100
+#define VERSION_MAJOR         1
+#define VERSION_MINOR         0
+#define VERSION_REVISION      0
 
 int main() {
   service::setup();
