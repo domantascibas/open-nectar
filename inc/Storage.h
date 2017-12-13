f2d515cbe6fabe4bcf99a18691afe0f16cff5241
Domantas Cibas <domantas@nectarsun.com>
2017-12-13 02:01:27 +0200

    adds flash storage class

---
+++ inc/Storage.h
@@ -0,0 +1,40 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+#include <stdint.h>
+#include "menu_service.h"
+#include "DataService.h"
+#include "localization.h"
+
+//data stored in memory address 0x0802E000
+
+struct StoredItem {
+  StoredItem(const uint16_t address) 
+    : lo_address(address), hi_address(address + 0x100) {};
+  void save(uint16_t);
+  uint16_t load();
+  
+private:
+  const uint16_t lo_address;
+  const uint16_t hi_address;
+  
+  uint16_t storedItem;
+};
+
+namespace Storage {
+  void init();
+  bool isConfigured();
+  void saveConfig();
+  void clearConfig();
+  void loadConfigData();
+  
+  void saveTemp(const TemperatureType &, const int8_t &);
+  void saveTime(const time_hm &, const TimeType &);
+  void saveLanguage(const Language &);
+  void saveHeaterMode(nectar_contract::HeaterMode);
+  
+  time_hm loadTime(const TimeType &);
+};
+
+#endif
+
+// *******************************Nectar Sun Copyright ɠNectar Sun 2017*************************************   
