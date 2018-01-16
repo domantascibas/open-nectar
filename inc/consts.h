99c09f1bda4803fb1a8185a9adf0f007c137329a
paplaukias <domantas.cibas@gmail.com>
2018-01-16 11:04:26 +0200

    adds consts.h file and updates the NectarSerial submodule

---
+++ inc/consts.h
@@ -0,0 +1,8 @@
+#ifndef CONSTS_H
+#define CONSTS_H
+
+#define NECTAR_POWER_BOARD_VERSION 103
+
+#endif
+
+// *******************************Nectar Sun Copyright ɠNectar Sun 2017*************************************   
