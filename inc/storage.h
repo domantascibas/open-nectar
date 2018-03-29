3312f2b794087a5f90f06befe96116ae34409aee
Domantas Cibas <domantas@nectarsun.com>
2018-03-29 16:44:03 +0300

    adds function wrappers to save and load our data to/from the flash

--- inc/storage.h
+++ inc/storage.h
@@ -1,25 +1,8 @@
 #ifndef STORAGE_H
 #define STORAGE_H
 #include <stdint.h>
-
-//data stored in memory address 0x08002000
-
-struct StoredItem {
-  StoredItem(const uint16_t address) 
-    : lo_address(address), hi_address(address + 0x100) {};
-  void load(float *);
-  void read(float *);
-  void save(float);
-
-private:
-  const uint16_t lo_address;
-  const uint16_t hi_address;
-
-  union {
-    float f;
-    uint16_t b[2];
-  } u;
-};
+#include "data.h"
+#include "eeprom.h"
 
 namespace Storage {
   void init();
