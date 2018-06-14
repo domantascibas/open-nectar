9d93c8cc5afc434bd0e9564d57eda2d2fca31568
Domantas Cibas <domantas@nectarsun.com>
2018-06-14 11:56:59 +0300

    increases readability

--- src/RelayController.cpp
+++ src/RelayController.cpp
@@ -29,10 +29,15 @@ uint8_t RelayController::getRelayState() {
 
 void RelayController::setRelays(uint8_t state) {
   relayState = state;
-  if(mainBoardError.has_error(NO_BOILER_TEMP) ||mainBoardError.has_error(DEVICE_OVERHEAT) || mainBoardError.has_error(MIN_TEMPERATURE) || mainBoardError.has_error(MAX_TEMPERATURE)) {
-    relayState = TURN_OFF_ALL;
-    printf("overheat/temperature-related errors\r\n");
-  }
+	if(mainBoardError.has_error(NO_BOILER_TEMP)
+			|| mainBoardError.has_error(DEVICE_OVERHEAT)
+			|| mainBoardError.has_error(MIN_TEMPERATURE)
+			|| mainBoardError.has_error(MAX_TEMPERATURE)
+	)	{
+		relayState = TURN_OFF_ALL;
+		printf("overheat/temperature-related errors\r\n");
+	}
+
   switch(relayState) {
     default:
     case TURN_OFF_ALL:
