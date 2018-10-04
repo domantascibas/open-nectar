6057f388f494d850a38f99e92db83b38537a306e
Domantas Cibas <domantas@nectarsun.com>
2018-10-04 17:24:02 +0300

    removes three init errors
    removes MAX_TEMPERATURE, MIN_TEMPERATURE and PROCESSOR_OVERHEAT errors from the init errors list

--- src/error_controller.cpp
+++ src/error_controller.cpp
@@ -6,8 +6,8 @@ ErrorHandler powerBoardError;
 
 void error_controller_init(void) {
 	mainBoardError.set_error(FLASH_ACCESS_ERROR);
-	mainBoardError.set_error(MAX_TEMPERATURE);
-	mainBoardError.set_error(MIN_TEMPERATURE);
+//	mainBoardError.set_error(MAX_TEMPERATURE);
+//	mainBoardError.set_error(MIN_TEMPERATURE);
 	mainBoardError.set_error(NO_BOILER_TEMP);
-	mainBoardError.set_error(PROCESSOR_OVERHEAT);
+//	mainBoardError.set_error(PROCESSOR_OVERHEAT);
 }
