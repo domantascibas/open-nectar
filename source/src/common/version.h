7704abe45cf61d2ec17d85e44a337b23a506f98c
Domantas Cibas <domantas.cibas@ruptela.com>
2019-11-27 17:09:23 +0200

    add version file

---
+++ source/src/common/version.h
@@ -0,0 +1,11 @@
+#ifndef __VERSION_H
+#define __VERSION_H
+
+#define HW_VER_LEN              15
+#define DEV_NAME_LEN            15
+
+extern const char version[12];
+extern const char HW_sign[4];
+extern const char Dev_Model[DEV_NAME_LEN];
+
+#endif
