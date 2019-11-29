933d78e848797ca3d6ecdff233f8773abe3203c3
Domantas Cibas <domantas.cibas@ruptela.com>
2019-11-29 15:58:08 +0200

    removes nucleo_helpers submodule

---
+++ source/src/target-specific/NS-power/watchdog_timer.cpp
@@ -0,0 +1,18 @@
+#include "mbed.h"
+#include "watchdog_timer.h"
+
+void watchdog_timer_init(void) {
+  IWDG->KR = 0xCCCC;
+  IWDG->KR = 0x5555;
+  IWDG->PR = IWDG_PR_PR_0;
+  IWDG->RLR = 0xFFFF;
+  while (IWDG->SR) {
+  /* add time out here for a robust application */
+  }
+  IWDG->KR = 0xAAAA;
+}
+
+void watchdog_timer_update(void) {
+  // #kickTheDog
+  IWDG->KR = 0xAAAA;
+}
