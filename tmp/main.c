6b6e477657595e922ce3be1b38668bced6a79a76
Domantas Cibas <domantas.cibas@ruptela.com>
2019-12-02 14:15:56 +0200

    tmp test

--- tmp/main.c
+++ tmp/main.c
@@ -7,13 +7,15 @@
 /* USER FUNCTIONS */
 
 void pwr_check(statusDataType_t status) {
-    uint8_t i;
+    uint8_t i, r;
     for (i = 1; i < 16; ) {
         PowerData_write(STATUS, &i);
         // printf("DATA_WRITE %d , GET_STATUS %d , CLEAR_STATUS %d , SET_STATUS %d\n\r", i, GET_STATUS(CALIBRATION_STATUS), CLEAR_STATUS(CALIBRATION_STATUS), SET_STATUS(CALIBRATION_STATUS));
-        printf("i: %d , VAL: %d , STATUS: %d , GET_STATUS(): %d\n\r", i, *PowerData_getStatusPtr(), status, GET_STATUS(status));
-        if (GET_STATUS(status) != (i & (1UL << status) >> status)) {
-            printf("[ERROR] GET_STATUS(): %d != (%d << %d) >> %d", GET_STATUS(status), i, status, status);
+        PowerData_read(STATUS, &r);
+        printf("i: %d , VAL: %d , READ_DATA(): %d , STATUS: %d , GET_STATUS(): %d\n\r", i, *PowerData_getStatusPtr(), r, status, GET_STATUS(status));
+        if (GET_STATUS(status) != ((i & (1UL << status)) >> status)) {
+            printf("[ERROR] GET_STATUS(): %d != (%d << %d) >> %d\n\n\r", GET_STATUS(status), i, status, status);
+            return;
         }
         i *= 2;
     }
@@ -25,11 +27,13 @@ void pwr_setStatus(statusDataType_t status) {
     // printf("DATA_WRITE %d , GET_STATUS %d , CLEAR_STATUS %d , SET_STATUS %d\n\r", i, GET_STATUS(CALIBRATION_STATUS), CLEAR_STATUS(CALIBRATION_STATUS), SET_STATUS(CALIBRATION_STATUS));
     printf("VAL: %d , STATUS: %d , GET_STATUS(): %d\n\r", *PowerData_getStatusPtr(), status, GET_STATUS(status));
     if (GET_STATUS(status) != 1) {
-        printf("[ERROR] SET_STATUS(): %d != 1", GET_STATUS(status));
+        printf("[ERROR] SET_STATUS(): %d != 1\n\n\r", GET_STATUS(status));
+        return;
     }
     CLEAR_STATUS(status);
     if (GET_STATUS(status) != 0) {
-        printf("[ERROR] GET_STATUS(): %d != 1", GET_STATUS(status));
+        printf("[ERROR] GET_STATUS(): %d != 1\n\n\r", GET_STATUS(status));
+        return;
     }
     printf("VAL: %d , STATUS: %d , GET_STATUS(): %d\n\r", *PowerData_getStatusPtr(), status, GET_STATUS(status));
     printf("\n\r");
@@ -57,8 +61,8 @@ void loop(void) {
     /* USER CODE START */
 
     for (uint8_t i = 0; i < 4; i++) {
-        // pwr_check((statusDataType_t)i);
-        pwr_setStatus((statusDataType_t)i);
+        pwr_check((statusDataType_t)i);
+        // pwr_setStatus((statusDataType_t)i);
     }
 
     /* USER CODE END */
