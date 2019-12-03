0cca08da335eab65a0979e7ee60cf79905c0158e
Domantas Cibas <domantas.cibas@ruptela.com>
2019-12-03 17:40:57 +0200

    simple test

---
+++ source/NS-project/test/AllTests/PowerDataTest.cpp
@@ -0,0 +1,26 @@
+#include "CppUTest/TestHarness.h"
+
+extern "C" {
+	/*
+	 * Add your c-only include files here
+	 */
+    // #include "data.h"
+    #include "consts.h"
+}
+
+TEST_GROUP(CleanExit) {
+    void setup() {
+        // PowerData_init();
+    }
+
+    void teardown() {
+        // PowerData_init();
+    }
+};
+
+TEST(CleanExit, GET_STATUS_GENERATOR) {
+    CHECK_EQUAL(110, NECTAR_POWER_BOARD_VERSION);
+    // uint8_t status = (uint8_t)GENERATOR_STATUS;
+    // PowerData_write(STATUS, &status);
+    // CHECK_TEXT(GET_STATUS(GENERATOR_STATUS), "GENERATOR_STATUS SET");
+}
