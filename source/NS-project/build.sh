e937812fa14887c2ff58c52634b0772cacdfca86
Domantas Cibas <domantas.cibas@ruptela.com>
2019-12-17 14:52:15 +0200

    update build scripts

--- source/NS-project/build.sh
+++ source/NS-project/build.sh
@@ -43,16 +43,16 @@ source/src/target-specific/NS-main/*" >> ./.mbedignore
 mbed compile
 
 # create bin
-mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.csv ./source/NS-project/bin/NS-power/
-mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.html ./source/NS-project/bin/NS-power/
-mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.json ./source/NS-project/bin/NS-power/
-mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.elf ./source/NS-project/bin/NS-power/
+mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.csv ./source/NS-project/bin/NS-power/power_board.csv
+mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.html ./source/NS-project/bin/NS-power/power_board.html
+mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.json ./source/NS-project/bin/NS-power/power_board.json
+mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.elf ./source/NS-project/bin/NS-power/power_board.elf
+
 /opt/xPacks/arm-none-eabi-gcc/8.3.1-1.3/bin/arm-none-eabi-objcopy -O ihex ./source/NS-project/bin/NS-power/power_board.elf ./source/NS-project/bin/NS-power/power_board.hex
 /opt/xPacks/arm-none-eabi-gcc/8.3.1-1.3/bin/arm-none-eabi-objcopy -O binary ./source/NS-project/bin/NS-power/power_board.elf ./source/NS-project/bin/NS-power/power_board.bin
 /opt/xPacks/arm-none-eabi-gcc/8.3.1-1.3/bin/arm-none-eabi-size ./source/NS-project/bin/NS-power/power_board.elf
 /opt/xPacks/arm-none-eabi-gcc/8.3.1-1.3/bin/arm-none-eabi-size ./source/NS-project/bin/NS-power/power_board.hex
 
-remove BUILD
+# remove BUILD
 rm -r BUILD
-
 rm -f ./.mbedignore
--- source/NS-project/build_main.sh
+++ source/NS-project/build_main.sh
@@ -45,16 +45,16 @@ source/src/target-specific/NS-power/*" >> ./.mbedignore
 mbed compile
 
 # create bin
-mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.csv ./source/NS-project/bin/NS-main/
-mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.html ./source/NS-project/bin/NS-main/
-mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.json ./source/NS-project/bin/NS-main/
-mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.elf ./source/NS-project/bin/NS-main/
+mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.csv ./source/NS-project/bin/NS-main/main_board.csv
+mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.html ./source/NS-project/bin/NS-main/main_board.html
+mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.json ./source/NS-project/bin/NS-main/main_board.json
+mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.elf ./source/NS-project/bin/NS-main/main_board.elf
+
 /opt/xPacks/arm-none-eabi-gcc/8.3.1-1.3/bin/arm-none-eabi-objcopy -O ihex ./source/NS-project/bin/NS-main/main_board.elf ./source/NS-project/bin/NS-main/main_board.hex
 /opt/xPacks/arm-none-eabi-gcc/8.3.1-1.3/bin/arm-none-eabi-objcopy -O binary ./source/NS-project/bin/NS-main/main_board.elf ./source/NS-project/bin/NS-main/main_board.bin
 /opt/xPacks/arm-none-eabi-gcc/8.3.1-1.3/bin/arm-none-eabi-size ./source/NS-project/bin/NS-main/main_board.elf
 /opt/xPacks/arm-none-eabi-gcc/8.3.1-1.3/bin/arm-none-eabi-size ./source/NS-project/bin/NS-main/main_board.hex
 
-remove BUILD
+# remove BUILD
 rm -r BUILD
-
 rm -f ./.mbedignore
