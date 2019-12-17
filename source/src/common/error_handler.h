d8b22c807c0f6765dfeaedc5494d4e0b9c347a64
Domantas Cibas <domantas@nectarsun.com>
2019-12-17 19:52:24 +0200

    format

--- source/src/common/error_handler.h
+++ source/src/common/error_handler.h
@@ -6,21 +6,21 @@
 
 struct ErrorHandler {
 public:
-  ErrorHandler();
-  void set_error(ERROR_CODE);
-  void get_errors(uint32_t *);
-  uint32_t get_errors();
-  void save_error_code(uint32_t);
-  void get_last_error(ERROR_CODE *);
-  void clear_error();
-  void clear_error(ERROR_CODE);
-  bool has_error(ERROR_CODE);
-  bool has_errors;
-    
+    ErrorHandler();
+    void set_error(ERROR_CODE);
+    void get_errors(uint32_t *);
+    uint32_t get_errors();
+    void save_error_code(uint32_t);
+    void get_last_error(ERROR_CODE *);
+    void clear_error();
+    void clear_error(ERROR_CODE);
+    bool has_error(ERROR_CODE);
+    bool has_errors;
+
 private:
-  uint32_t raised_errors;
-  ERROR_CODE last_error;
-  void print_error(ERROR_CODE *);
+    uint32_t raised_errors;
+    ERROR_CODE last_error;
+    void print_error(ERROR_CODE *);
 };
 
 #endif
