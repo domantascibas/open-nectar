a2db988b51d1f101e9ff2016656aea1011af27cc
Domantas Cibas <domantas@nectarsun.com>
2019-12-21 13:23:38 +0200

    fix type
    # Conflicts:
    #	source/src/target-specific/NS-main/relay-state/RelayState.h

---
+++ source/src/target-specific/NS-main/relay-state/RelayState.h
@@ -0,0 +1,28 @@
+#ifndef __RELAY_STATE_H
+#define __RELAY_STATE_H
+
+#include <stdint.h>
+
+typedef enum {
+    SUN_RELAY,
+    GRID_RELAY
+} relayType_t;
+
+typedef enum {
+    OFF,
+    ON,
+    SWITCHING
+} relayStateType_t;
+
+typedef struct {
+    relayStateType_t state;
+    relayStateType_t state_next;
+    // uint8_t is_switching;
+} relayControllerType_t;
+
+void RelayState_init(void);
+uint8_t RelayState_run(void);
+uint8_t RelayState_set(relayType_t type, relayStateType_t state);
+relayStateType_t RelayState_get(relayType_t type);
+
+#endif
