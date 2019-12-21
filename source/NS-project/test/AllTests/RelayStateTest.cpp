d45bf5f44f0b63e6bd76f4447a2c217d2b2fb53f
Domantas Cibas <domantas@nectarsun.com>
2019-12-21 13:03:27 +0200

    relayState Tests group
    # Conflicts:
    #	source/NS-project/test/AllTests/RelayStateTest.cpp

---
+++ source/NS-project/test/AllTests/RelayStateTest.cpp
@@ -0,0 +1,17 @@
+#include "CppUTest/TestHarness.h"
+#include <stdint.h>
+#include <stdio.h>
+
+extern "C" {
+#include "RelayState.h"
+}
+
+TEST_GROUP(RelayStateTests) {
+    void setup() {
+    }
+
+    void teardown() {
+    }
+};
+
+#endif
