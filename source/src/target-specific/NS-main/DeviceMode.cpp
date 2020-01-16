11d93dc008470b3cefca4f32c9626208cdbb43ad
Domantas Cibas <domantas.cibas@ruptela.com>
2020-01-16 18:09:41 +0200

    device mode restore

--- source/src/target-specific/NS-main/DeviceMode.cpp
+++ source/src/target-specific/NS-main/DeviceMode.cpp
@@ -3,8 +3,6 @@
 #include "Storage.h"
 #include "error_controller.h"
 
-// DeviceMode deviceOpMode;
-
 static opModes currentMode;
 static uint8_t testMode;
 static uint8_t onboarding;
@@ -31,19 +29,9 @@ uint8_t deviceOpMode_getCurrentMode(void) {
   return currentMode;
 }
 
-uint8_t DeviceMode::getCurrentMode() {
-  // return currentMode;
-  return 0;
-}
-
 void deviceOpMode_setConfigured(void) {
   currentMode = CONFIGURED;
   configured = 1;
-}
-
-void DeviceMode::setConfigured() {
-  // currentMode = CONFIGURED;
-  // configured = true;
 }
 
 void deviceOpMode_resetConfiguration(void) {
@@ -51,27 +39,14 @@ void deviceOpMode_resetConfiguration(void) {
   testMode = 0;
   configured = 0;
   onboarding = 1;
-  // device_modes::reset();
-  // Storage::clearConfig();
-}
-
-void DeviceMode::resetConfiguration() {
-  // currentMode = WELCOME;
-  // testMode = false;
-  // configured = false;
-  // onboarding = true;
+  device_modes_reset();
+  storage_clearConfig();
 }
 
 void deviceOpMode_setTestMode(void) {
   testMode = 1;
   deviceOpMode_endOnboarding();
   currentMode = TEST_MODE;
-}
-
-void DeviceMode::setTestMode() {
-  // testMode = true;
-  // endOnboarding();
-  // currentMode = TEST_MODE;
 }
 
 void deviceOpMode_endOnboarding(void) {
@@ -82,15 +57,8 @@ void deviceOpMode_endOnboarding(void) {
   }
   onboarding = false;
   if (!testMode) {
-    // Storage::saveConfig();
+    storage_saveConfig();
   }
-}
-
-void DeviceMode::endOnboarding() {
-  // if(!configured) currentMode = NOT_CONFIGURED;
-  // else currentMode = CONFIGURED;
-  // onboarding = false;
-  // if(!testMode) Storage::saveConfig();
 }
 
 void deviceOpMode_setInTestStand(void) {
@@ -98,111 +66,50 @@ void deviceOpMode_setInTestStand(void) {
 	mainBoardError.clear_error(NO_BOILER_TEMP);
 }
 
-void DeviceMode::setInTestStand() {
-  // inTestStand = true;
-	// mainBoardError.clear_error(NO_BOILER_TEMP);
-}
-
 uint8_t deviceOpMode_isInTestMode(void) {
   return testMode;
-}
-
-bool DeviceMode::isInTestMode() {
-  // return testMode;
-  return 0;
 }
 
 uint8_t deviceOpMode_isOnboarding(void) {
   return onboarding;
 }
 
-bool DeviceMode::isOnboarding() {
-  // return onboarding;
-  return 0;
-}
-
 uint8_t deviceOpMode_isConfigured(void) {
   return configured;
-}
-
-bool DeviceMode::isConfigured() {
-  // return configured;
-  return 0;
 }
 
 uint8_t deviceOpMode_isPairing(void) {
   return pairing;
 }
 
-bool DeviceMode::isPairing() {
-  // return pairing;
-  return 0;
-}
-
 uint8_t deviceOpMode_isReset(void) {
   return reset;
-}
-
-bool DeviceMode::isReset() {
-  // return reset;
-  return 0;
 }
 
 uint8_t deviceOpMode_isBoostOff(void) {
   return boostOff;
 }
 
-bool DeviceMode::isBoostOff() {
-  // return boostOff;
-  return 0;
-}
-
 uint8_t deviceOpMode_isInTestStand(void) {
   return inTestStand;
-}
-
-bool DeviceMode::isInTestStand() {
-  // return inTestStand;
-  return 0;
 }
 
 uint8_t deviceOpMode_isLoading(void) {
   return loading;
 }
 
-bool DeviceMode::isLoading() {
-  // return loading;
-  return 0;
-}
-
 void deviceOpMode_endLoading(void) {
   loading = false;
-}
-
-void DeviceMode::endLoading() {
-  // loading = false;
 }
 
 void deviceOpMode_setPairing(uint8_t input) {
   pairing = input;
 }
 
-void DeviceMode::setPairing(bool input) {
-  // pairing = input;
-}
-
 void deviceOpMode_setReset(uint8_t input) {
   reset = input;
 }
 
-void DeviceMode::setReset(bool input) {
-  // reset = input;
-}
-
 void deviceOpMode_setBoostOff(uint8_t input) {
   boostOff = input;
-}
-
-void DeviceMode::setBoostOff(bool input) {
-  // boostOff = input;
 }
--- source/src/target-specific/NS-main/DeviceMode.h
+++ source/src/target-specific/NS-main/DeviceMode.h
@@ -30,41 +30,4 @@ void deviceOpMode_setPairing(uint8_t input);
 void deviceOpMode_setReset(uint8_t input);
 void deviceOpMode_setBoostOff(uint8_t input);
 
-struct DeviceMode {
-    DeviceMode() : currentMode(WELCOME), testMode(false), onboarding(true), configured(false), pairing(true), reset(false), boostOff(false), inTestStand(false), loading(true) {};
-    uint8_t getCurrentMode();
-    void endOnboarding();
-    void setTestMode();
-    void setConfigured();
-    void resetConfiguration();
-    void setInTestStand();
-
-    bool isInTestMode();
-    bool isOnboarding();
-    bool isConfigured();
-    bool isPairing();
-    bool isReset();
-    bool isBoostOff();
-    bool isInTestStand();
-    bool isLoading();
-
-    void endLoading();
-    void setPairing(bool);
-    void setReset(bool);
-    void setBoostOff(bool);
-
-private:
-    opModes currentMode;
-    bool testMode;
-    bool onboarding;
-    bool configured;
-    bool pairing;
-    bool reset;
-    bool boostOff;
-    bool inTestStand;
-    bool loading;
-};
-
-extern DeviceMode deviceOpMode;
-
 #endif
