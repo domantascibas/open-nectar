3024c9a6626d219b067d02d3b0ea320f34bb2468
Domantas Cibas <domantas.cibas@ruptela.com>
2020-01-16 16:16:31 +0200

    refactor deviceOpMode functions

--- source/src/target-specific/NS-main/DeviceMode.cpp
+++ source/src/target-specific/NS-main/DeviceMode.cpp
@@ -5,9 +5,40 @@
 
 // DeviceMode deviceOpMode;
 
+static opModes currentMode;
+static uint8_t testMode;
+static uint8_t onboarding;
+static uint8_t configured;
+static uint8_t pairing;
+static uint8_t reset;
+static uint8_t boostOff;
+static uint8_t inTestStand;
+static uint8_t loading;
+
+void deviceOpMode_init(void) {
+  currentMode = WELCOME;
+  testMode = 0;
+  onboarding = 1;
+  configured = 0;
+  pairing = 1;
+  reset = 0;
+  boostOff = 0;
+  inTestStand = 0;
+  loading = 1;
+}
+
+uint8_t deviceOpMode_getCurrentMode(void) {
+  return currentMode;
+}
+
 uint8_t DeviceMode::getCurrentMode() {
   // return currentMode;
   return 0;
+}
+
+void deviceOpMode_setConfigured(void) {
+  currentMode = CONFIGURED;
+  configured = 1;
 }
 
 void DeviceMode::setConfigured() {
@@ -15,19 +46,44 @@ void DeviceMode::setConfigured() {
   // configured = true;
 }
 
+void deviceOpMode_resetConfiguration(void) {
+  currentMode = WELCOME;
+  testMode = 0;
+  configured = 0;
+  onboarding = 1;
+  // device_modes::reset();
+  // Storage::clearConfig();
+}
+
 void DeviceMode::resetConfiguration() {
   // currentMode = WELCOME;
   // testMode = false;
   // configured = false;
   // onboarding = true;
-  // device_modes::reset();
-  // Storage::clearConfig();
+}
+
+void deviceOpMode_setTestMode(void) {
+  testMode = 1;
+  deviceOpMode_endOnboarding();
+  currentMode = TEST_MODE;
 }
 
 void DeviceMode::setTestMode() {
   // testMode = true;
   // endOnboarding();
   // currentMode = TEST_MODE;
+}
+
+void deviceOpMode_endOnboarding(void) {
+  if (!configured) {
+    currentMode = NOT_CONFIGURED;
+  } else {
+    currentMode = CONFIGURED;
+  }
+  onboarding = false;
+  if (!testMode) {
+    // Storage::saveConfig();
+  }
 }
 
 void DeviceMode::endOnboarding() {
@@ -37,9 +93,18 @@ void DeviceMode::endOnboarding() {
   // if(!testMode) Storage::saveConfig();
 }
 
+void deviceOpMode_setInTestStand(void) {
+  inTestStand = 1;
+	mainBoardError.clear_error(NO_BOILER_TEMP);
+}
+
 void DeviceMode::setInTestStand() {
   // inTestStand = true;
 	// mainBoardError.clear_error(NO_BOILER_TEMP);
+}
+
+uint8_t deviceOpMode_isInTestMode(void) {
+  return testMode;
 }
 
 bool DeviceMode::isInTestMode() {
@@ -47,9 +112,17 @@ bool DeviceMode::isInTestMode() {
   return 0;
 }
 
+uint8_t deviceOpMode_isOnboarding(void) {
+  return onboarding;
+}
+
 bool DeviceMode::isOnboarding() {
   // return onboarding;
   return 0;
+}
+
+uint8_t deviceOpMode_isConfigured(void) {
+  return configured;
 }
 
 bool DeviceMode::isConfigured() {
@@ -57,9 +130,17 @@ bool DeviceMode::isConfigured() {
   return 0;
 }
 
+uint8_t deviceOpMode_isPairing(void) {
+  return pairing;
+}
+
 bool DeviceMode::isPairing() {
   // return pairing;
   return 0;
+}
+
+uint8_t deviceOpMode_isReset(void) {
+  return reset;
 }
 
 bool DeviceMode::isReset() {
@@ -67,9 +148,17 @@ bool DeviceMode::isReset() {
   return 0;
 }
 
+uint8_t deviceOpMode_isBoostOff(void) {
+  return boostOff;
+}
+
 bool DeviceMode::isBoostOff() {
   // return boostOff;
   return 0;
+}
+
+uint8_t deviceOpMode_isInTestStand(void) {
+  return inTestStand;
 }
 
 bool DeviceMode::isInTestStand() {
@@ -77,21 +166,41 @@ bool DeviceMode::isInTestStand() {
   return 0;
 }
 
+uint8_t deviceOpMode_isLoading(void) {
+  return loading;
+}
+
 bool DeviceMode::isLoading() {
   // return loading;
   return 0;
+}
+
+void deviceOpMode_endLoading(void) {
+  loading = false;
 }
 
 void DeviceMode::endLoading() {
   // loading = false;
 }
 
+void deviceOpMode_setPairing(uint8_t input) {
+  pairing = input;
+}
+
 void DeviceMode::setPairing(bool input) {
   // pairing = input;
 }
 
+void deviceOpMode_setReset(uint8_t input) {
+  reset = input;
+}
+
 void DeviceMode::setReset(bool input) {
   // reset = input;
+}
+
+void deviceOpMode_setBoostOff(uint8_t input) {
+  boostOff = input;
 }
 
 void DeviceMode::setBoostOff(bool input) {
--- source/src/target-specific/NS-main/DeviceMode.h
+++ source/src/target-specific/NS-main/DeviceMode.h
@@ -3,12 +3,32 @@
 
 #include <stdint.h>
 
-enum opModes {
+typedef enum {
     WELCOME,
     NOT_CONFIGURED,
     CONFIGURED,
     TEST_MODE
-};
+} opModes;
+
+void deviceOpMode_init(void);
+uint8_t deviceOpMode_getCurrentMode(void);
+void deviceOpMode_setConfigured(void);
+void deviceOpMode_resetConfiguration(void);
+void deviceOpMode_setTestMode(void);
+void deviceOpMode_endOnboarding(void);
+void deviceOpMode_setInTestStand(void);
+uint8_t deviceOpMode_isInTestMode(void);
+uint8_t deviceOpMode_isOnboarding(void);
+uint8_t deviceOpMode_isConfigured(void);
+uint8_t deviceOpMode_isPairing(void);
+uint8_t deviceOpMode_isReset(void);
+uint8_t deviceOpMode_isBoostOff(void);
+uint8_t deviceOpMode_isInTestStand(void);
+uint8_t deviceOpMode_isLoading(void);
+void deviceOpMode_endLoading(void);
+void deviceOpMode_setPairing(uint8_t input);
+void deviceOpMode_setReset(uint8_t input);
+void deviceOpMode_setBoostOff(uint8_t input);
 
 struct DeviceMode {
     DeviceMode() : currentMode(WELCOME), testMode(false), onboarding(true), configured(false), pairing(true), reset(false), boostOff(false), inTestStand(false), loading(true) {};
