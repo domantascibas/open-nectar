5bc3e1b2cb6e7dd8c4bfe8a394d0127215219982
Domantas Cibas <domantas.cibas@ruptela.com>
2020-01-16 13:04:24 +0200

    refactor temperature sensor to not use c++ objects

--- source/src/target-specific/NS-main/TemperatureSensor.cpp
+++ source/src/target-specific/NS-main/TemperatureSensor.cpp
@@ -1,93 +1,102 @@
-#include "TemperatureSensor.h"
-#include "error_controller.h"
+// #include "mbed.h"
 #include "consts.h"
+#include "pins.h"
+#include "DS1820.h"
+#include "TemperatureSensor.h"
+// #include "error_controller.h"
 
-TemperatureSensor::TemperatureSensor(PinName pin, uint8_t refresh)
-: probe(pin), newValueAvailable(false), sensorFound(false), refreshRate(refresh) {
+DS1820 temp_probe(BOILER_TEMP_PIN);
+// Ticker ticker;
+// Timeout timeout;
+
+uint8_t temperature;
+uint8_t last_temperature;
+uint8_t error_counter;
+uint8_t sensor_found;
+uint8_t identical_count;
+uint8_t refresh_rate;
+uint8_t new_value_avail;
+
+// must implement state machine with 4 states:
+// - idle
+// - start conversion
+// - wait for conversion end
+// - temperature ready
+
+void temperatureSensor_init(void) {
+	temperature = 0.0;
+	last_temperature = 0.0;
+	error_counter = 0;
+	if(temp_probe.begin()) {
+		sensor_found = 1;
+		// temperatureSensor_start();
+		// attachTicker(refreshRate);
+	}
+//    printf("Temperature sensor found\r\n");
+//  } else printf("Temperature sensor not found\r\n");
 }
 
-void TemperatureSensor::init() {
-// 	temperature = 0.0;
-// 	last_temperature = 0.0;
-// 	errorCounter = 0;
-//   if(probe.begin()) {
-//     sensorFound = true;
-//     measureTemperature();
-//     attachTicker(refreshRate);
-// 	}
-// //    printf("Temperature sensor found\r\n");
-// //  } else printf("Temperature sensor not found\r\n");
+uint8_t temperatureSensor_isFound(void) {
+	return sensor_found;
 }
 
-bool TemperatureSensor::isSensorFound() {
-//   return sensorFound;
-	return 1;
+uint8_t temperatureSensor_isNewValAvail(void) {
+	return new_value_avail;
 }
 
-bool TemperatureSensor::isNewValueAvailable() {
-//   return newValueAvailable;
-	return 1;
-}
-
-float TemperatureSensor::getTemperature() {
-//   newValueAvailable = false;
-//   return temperature;
+uint8_t temperatureSensor_get(void) {
+	new_value_avail = 0;
+	// return temperature;
 	return 85;
 }
 
-void TemperatureSensor::attachTicker(float interval) {
-//   ticker.attach(callback(this, &TemperatureSensor::measureTemperature), interval);
+uint8_t temperatureSensor_start(void) {
+	temp_probe.startConversion();
 }
 
-void TemperatureSensor::detachTicker() {
-//   ticker.detach();
-}
-
-void TemperatureSensor::measureTemperature() {
-//   probe.startConversion();
-//   timeout.attach(callback(this, &TemperatureSensor::readTemperatureToStorage), 0.75);
-}
-
-void TemperatureSensor::readTemperatureToStorage() {
+void temperatureSensor_read(void) {
 	// if((identical_count >= LOST_SENSOR_COUNT) || mainBoardError.has_error(NO_BOILER_TEMP)) {
-	// 	if(probe.begin()) {
-	// 		sensorFound = true;
-	// 		identical_count = 0;
-	// 	} else {
-	// 		sensorFound = false;
-	// 		identical_count = LOST_SENSOR_COUNT;
-	// 	}
-	// }
-	
-	// if(sensorFound) {
-	// 	last_temperature = temperature;
-	// 	probe.read(temperature);
-		
-	// 	if(temperature == last_temperature) {
-	// 		identical_count++;
-	// 	} else {
-	// 		identical_count = 0;
-	// 	}
-	
-	// 	if(temperature != 85.00) {
-	// 		if(mainBoardError.has_error(NO_BOILER_TEMP)) {
-	// 			mainBoardError.clear_error(NO_BOILER_TEMP);
-	// 		}
-	// 		errorCounter = 0;
-	// 		newValueAvailable = true;
-	// 	} else {
-	// 		errorCounter++;
-	// 	}
-		
-	// 	if(errorCounter >= 10) {
-	// 		errorCounter = 10;
-	// 		temperature = 0.0;
-	// 		mainBoardError.set_error(NO_BOILER_TEMP);
-	// 		newValueAvailable = true;
-	// 	}
-	// } else {
-	// 	temperature = 0.0;
-	// 	mainBoardError.set_error(NO_BOILER_TEMP);
-	// 	newValueAvailable = true;
-	// }
+	if (identical_count >= LOST_SENSOR_COUNT) {
+		if (temp_probe.begin()) {
+			sensor_found = 1;
+			identical_count = 0;
+		} else {
+			sensor_found = 0;
+			identical_count = LOST_SENSOR_COUNT;
+		}
+	}
+
+	if (sensor_found) {
+		float t;
+		last_temperature = temperature;
+		temp_probe.read(t);
+		temperature = (uint8_t)t;
+
+		if (temperature == last_temperature) {
+			identical_count++;
+		} else {
+			identical_count = 0;
+		}
+
+		if (temperature != 85) {
+			// if(mainBoardError.has_error(NO_BOILER_TEMP)) {
+			// 	mainBoardError.clear_error(NO_BOILER_TEMP);
+			// }
+			error_counter = 0;
+			new_value_avail = 1;
+		} else {
+			error_counter++;
+		}
+
+		if(error_counter >= 10) {
+			error_counter = 10;
+			temperature = 0;
+			// mainBoardError.set_error(NO_BOILER_TEMP);
+			new_value_avail = 1;
+		}
+	} else {
+		temperature = 0;
+		// mainBoardError.set_error(NO_BOILER_TEMP);
+		new_value_avail = 1;
+	}
 }
