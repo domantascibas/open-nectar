d44602b6aed14198516c10321da0f1798d4082d4
Domantas Cibas <domantas.cibas@ruptela.com>
2020-01-16 17:12:46 +0200

    run more code in main

--- source/src/target-specific/NS-main/main.cpp
+++ source/src/target-specific/NS-main/main.cpp
@@ -1,16 +1,16 @@
 #include "consts.h"
-#include "ServiceComms.h"
-#include "EspComms.h"
-#include "PowerBoardComms.h"
+// #include "ServiceComms.h"
+// #include "EspComms.h"
+// #include "PowerBoardComms.h"
 #include "device_modes.h"
 // #include "menu_service.h"
 #include "error_controller.h"
-// #include "TemperatureController.h"
-#include "TemperatureSensor.h"
-#include "DataService.h"
+#include "TemperatureController.h"
+// #include "DataService.h"
 #include "Storage.h"
-#include "CommsController.h"
+// #include "CommsController.h"
 #include "watchdog_timer.h"
+#include "DeviceMode.h"
 
 // bool inErrorScreen = false;
 
@@ -21,18 +21,18 @@ int main() {
 	float current_s;
   static bool isFirst = true;
 	error_controller_init();
-  // Storage::init();
+  deviceOpMode_init();
+  storage_init();
   
   // service::setup();
   
   // menu_service::setup();
   // menu_service::updateScreen();
-  // wait(1.0);
-  // tempController.init();
-  temperatureSensor_init();
-  // wait(0.5);
-  // tempController.updateTemperatures();
-  // wait(0.5);
+  wait(1.0);
+  temperatureController_init();
+  wait(0.5);
+  temperatureController_update();
+  wait(0.5);
 
 	// menu_service::updateScreen();
 	// wait(0.5);
@@ -40,50 +40,50 @@ int main() {
 	// wait(0.5);
 	// power_board::setup();
 	
-	// comms_timeout.start();
-  // current_s = comms_timeout.read();
+	comms_timeout.start();
+  current_s = comms_timeout.read();
 	// while((!power_board::receivedFirstMessage() || (powerData.power_version == 0.00)) && (current_s < POWER_INIT_TIMEOUT)) {
 	// 	__WFI();
 	// 	current_s = comms_timeout.read();
 	// }
-	// comms_timeout.stop();
-	// comms_timeout.reset();
+	comms_timeout.stop();
+	comms_timeout.reset();
 	// menu_service::updateScreen();
-  // wait(1.5);
+  wait(1.5);
 	
 	// menu_service::updateScreen();
-	// comms_timeout.start();
-	// current_s = comms_timeout.read();
+	comms_timeout.start();
+	current_s = comms_timeout.read();
   // while((!esp::receivedFirstMessage() || (espData.esp_version == 0.00)) && (current_s < ESP_INIT_TIMEOUT)) {
 	// 	__WFI();
 	// 	current_s = comms_timeout.read();
 	// }
-	// comms_timeout.stop();
-	// comms_timeout.reset();
+	comms_timeout.stop();
+	comms_timeout.reset();
 	// menu_service::updateScreen();
-  // wait(1.5);
+  wait(1.5);
   
 	// menu_service::updateScreen();
 	// device_modes::setup();
-  // wait(2);
+  wait(2);
 	
 	watchdog_timer_init();
-  // deviceOpMode.endLoading();
+  deviceOpMode_endLoading();
   
   while(1) {
     watchdog_timer_update();
     
     if(isFirst) {
       isFirst = false;
-  //     if(Storage::isConfigured()) {
-  //       printf("CONFIG loading data from storage\r\n");
-  //       Storage::loadConfigData();
-  //       if(Storage::isEspConfigured()) {
+      if(storage_isConfigured()) {
+        printf("CONFIG loading data from storage\r\n");
+        storage_loadConfigData();
+        if(storage_isEspConfigured()) {
   //         espData.is_configured = true;
-  //         deviceOpMode.setConfigured();
-  //       }
-  //       deviceOpMode.endOnboarding();
-  //     }
+          deviceOpMode_setConfigured();
+        }
+        deviceOpMode_endOnboarding();
+      }
   //     menu_service::needUpdate = true;
     }
     
@@ -91,9 +91,9 @@ int main() {
   //     menu_service::updateScreen();
   //   }
     
-  //   switch(deviceOpMode.getCurrentMode()) {
-  //     default:
-  //     case NOT_CONFIGURED:        // ESP NOT CONFIGURED
+    switch(deviceOpMode_getCurrentMode()) {
+      default:
+      case NOT_CONFIGURED:        // ESP NOT CONFIGURED
   //       if(espData.is_configured) {
   //         printf("CONFIG SAVE\n");
   //         deviceOpMode.setConfigured();
@@ -101,15 +101,15 @@ int main() {
   //         menu_service::resetScreen = true;
   //         Storage::saveEspConfig();
   //       }
-  //       break;
+        break;
       
-  //     case CONFIGURED:            // ESP CONFIGURED
+      case CONFIGURED:            // ESP CONFIGURED
   //       if(!espData.is_configured) {
   //         printf("CONFIG RESET\n");
   //       }
-  //       break;
+        break;
       
-  //     case WELCOME:
+      case WELCOME:
   //       //nothing to do here
   //       //wait for user to finish onboarding
   //       if(espData.is_configured || Storage::isEspConfigured()) {
@@ -118,20 +118,20 @@ int main() {
   //         menu_service::needUpdate = true;
   //         menu_service::resetScreen = true;
   //       }
-  //       break;
+        break;
       
-  //     case TEST_MODE:
+      case TEST_MODE:
   //       if(isFirst) {
   //         isFirst = false;
   //         deviceOpMode.setTestMode();
   //       }
-  //       break;
-  //   }
+        break;
+    }
     
-  //   tempController.updateTemperatures();
-  //   if(!deviceOpMode.isOnboarding()) {
-  //     device_modes::loop();
-  //   }
+    temperatureController_update();
+    if(!deviceOpMode_isOnboarding()) {
+      device_modes_loop();
+    }
     
   //   commsController.clearQueue();
     
