ea6e24c699e968e811f1ac4dd9172ab301c78fb2
Domantas Cibas <domantas.cibas@ruptela.com>
2020-01-16 17:55:34 +0200

    enable relays

--- source/src/target-specific/NS-main/relays/Relay.cpp
+++ source/src/target-specific/NS-main/relays/Relay.cpp
@@ -1,25 +1,25 @@
 #include "Relay.h"
 
 Relay::Relay(PinName pin) : relay(pin) {
-  // turnedOn = false;
-  // relayTurnOn = false;
-  // switching = false;
+  turnedOn = false;
+  relayTurnOn = false;
+  switching = false;
 }
 
 void Relay::relayOn() {
-  // turnedOn = true;
-  // relay = turnedOn;
+  turnedOn = true;
+  relay = turnedOn;
 }
 
 void Relay::relayOff() {
-  // turnedOn = false;
-  // relay = turnedOn;
+  turnedOn = false;
+  relay = turnedOn;
 }
 
 bool Relay::isOn() {
-  // return turnedOn;
+  return turnedOn;
 }
 
 bool Relay::isSwitching() {
-  // return switching;
+  return switching;
 }
--- source/src/target-specific/NS-main/relays/RelayController.cpp
+++ source/src/target-specific/NS-main/relays/RelayController.cpp
@@ -2,66 +2,66 @@
 #include "error_controller.h"
 #include "EnergyMeter.h"
 
-// GridRelay gridRelay;
-// SunRelay sunRelay;
+GridRelay gridRelay;
+SunRelay sunRelay;
 
 RelayController::RelayController() {
-  // relayState = TURN_OFF_ALL;
+  relayState = TURN_OFF_ALL;
 }
 
 void RelayController::init() {
-  // gridRelay.init();
-  // sunRelay.init();
-  // setRelays(TURN_OFF_ALL);
+  gridRelay.init();
+  sunRelay.init();
+  setRelays(TURN_OFF_ALL);
 }
 
 bool RelayController::isGridRelayOn() {
-  // return gridRelay.isOn();
+  return gridRelay.isOn();
 }
 
 bool RelayController::isSunRelayOn() {
-  // return sunRelay.isOn();
+  return sunRelay.isOn();
 }
 
 uint8_t RelayController::getRelayState() {
-  // return relayState;
+  return relayState;
 }
 
 void RelayController::setRelays(uint8_t state) {
-  // relayState = state;
+  relayState = state;
 	
-  // switch(relayState) {
-  //   default:
-  //   case TURN_OFF_ALL:
-  //     sunRelay.turnOff();
-  //     gridRelay.turnOff();
-  //     gridMeter.stopMeter(gridRelay.isOn());
-	//     printf("RELAYS ALL OFF\n");
-  //   break;
+  switch(relayState) {
+    default:
+    case TURN_OFF_ALL:
+      sunRelay.turnOff();
+      gridRelay.turnOff();
+      gridMeter.stopMeter(gridRelay.isOn());
+	    printf("RELAYS ALL OFF\n");
+    break;
 		
-  //   case TURN_OFF_SUN:
-  //     sunRelay.turnOff();
-  //     gridMeter.stopMeter(gridRelay.isOn());
-  //     printf("RELAYS SUN OFF\n");
-	//   break;
+    case TURN_OFF_SUN:
+      sunRelay.turnOff();
+      gridMeter.stopMeter(gridRelay.isOn());
+      printf("RELAYS SUN OFF\n");
+	  break;
     
-  //   case TURN_ON_SUN:
-  //     gridRelay.turnOff();
-  //     sunRelay.turnOn();
-  //     gridMeter.stopMeter(gridRelay.isOn());
-  //     printf("RELAYS SUN ON\n");
-  //   break;
+    case TURN_ON_SUN:
+      gridRelay.turnOff();
+      sunRelay.turnOn();
+      gridMeter.stopMeter(gridRelay.isOn());
+      printf("RELAYS SUN ON\n");
+    break;
     
-  //   case TURN_ON_GRID:
-  //     sunRelay.turnOff();
-  //     gridRelay.turnOn();
-  //     gridMeter.startMeter();
-  //     printf("RELAYS GRID ON\n");
-  //   break;
-  // }
+    case TURN_ON_GRID:
+      sunRelay.turnOff();
+      gridRelay.turnOn();
+      gridMeter.startMeter();
+      printf("RELAYS GRID ON\n");
+    break;
+  }
 }
 
 bool RelayController::finishedSwitching() {
-  // if(!gridRelay.isSwitching() && !sunRelay.isSwitching()) return true;
-  // else return false;
+  if(!gridRelay.isSwitching() && !sunRelay.isSwitching()) return true;
+  else return false;
 }
