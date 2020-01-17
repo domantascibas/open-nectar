6c6f29bb98f544f3bc22edc359a324f84f214f57
Domantas Cibas <domantas.cibas@ruptela.com>
2020-01-17 16:38:48 +0200

    add error_handler tests

---
+++ source/NS-project/test/AllTests/ErrorHandlerTest.cpp
@@ -0,0 +1,18 @@
+#include "CppUTest/TestHarness.h"
+#include <stdio.h>
+#include "error_handler.h"
+
+TEST_GROUP(ErrorHandler) {
+    void setup() {
+        printf("\r\n***** NS-ERROR TEST *****\r\n");
+        uint16_t err = (1U << NS_FLASH_ACCESS_MAIN) | (1U << NS_FLASH_ACCESS_POWER) | (1U << NS_ADC_CURRENT) | (1U << NS_ADC_VOLTAGE) | (1U << NS_CALIBRATION) | (1U << NS_MAX_TEMPERATURE) | (1U << NS_MIN_TEMPERATURE);
+        error_init(err);
+    }
+
+    void teardown() {
+    }
+};
+
+TEST(ErrorHandler, Init) {
+    error_print();
+}
