b38b09fddf2cdf0997461cf96ab63ef4cba9be8f
Domantas Cibas <domantas@nectarsun.com>
2020-04-16 12:08:19 +0300

    makefile update

--- source/NS-project/build_main.sh
+++ source/NS-project/build_main.sh
@@ -39,16 +39,12 @@ source/src/target-specific/NS-power/*
 mbed compile
 
 # create bin
-mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.csv ./source/NS-project/bin/NS-main/main_board.csv
-mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.html ./source/NS-project/bin/NS-main/main_board.html
-mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.json ./source/NS-project/bin/NS-main/main_board.json
-mv ./BUILD/NUCLEO_F091RC/GCC_ARM/*.elf ./source/NS-project/bin/NS-main/main_board.elf
+cp ./BUILD/NUCLEO_F091RC/GCC_ARM/*.csv ./source/NS-project/bin/NS-main/main_board.csv
+cp ./BUILD/NUCLEO_F091RC/GCC_ARM/*.html ./source/NS-project/bin/NS-main/main_board.html
+cp ./BUILD/NUCLEO_F091RC/GCC_ARM/*.json ./source/NS-project/bin/NS-main/main_board.json
+cp ./BUILD/NUCLEO_F091RC/GCC_ARM/*.elf ./source/NS-project/bin/NS-main/main_board.elf
 
 /opt/xPacks/arm-none-eabi-gcc/8.3.1-1.3/bin/arm-none-eabi-objcopy -O ihex ./source/NS-project/bin/NS-main/main_board.elf ./source/NS-project/bin/NS-main/main_board.hex
 /opt/xPacks/arm-none-eabi-gcc/8.3.1-1.3/bin/arm-none-eabi-objcopy -O binary ./source/NS-project/bin/NS-main/main_board.elf ./source/NS-project/bin/NS-main/main_board.bin
 /opt/xPacks/arm-none-eabi-gcc/8.3.1-1.3/bin/arm-none-eabi-size ./source/NS-project/bin/NS-main/main_board.elf
 /opt/xPacks/arm-none-eabi-gcc/8.3.1-1.3/bin/arm-none-eabi-size ./source/NS-project/bin/NS-main/main_board.hex
-
-# remove BUILD
-rm -r BUILD
-rm -f ./.mbedignore
