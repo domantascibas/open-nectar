0a3db5275719b6ee7ceec141c2884063cbc597d6
Domantas Cibas <domantas@nectarsun.com>
2020-04-20 22:16:28 +0300

    adapt to our project

--- source/src/modules/1wire/u1wire.c
+++ source/src/modules/1wire/u1wire.c
@@ -1,18 +1,5 @@
-/** @file   u1wire.c
-    @author M. P. Hayes
-    @date   16 May 20002
-    @brief  Low level routines to drive Dallas universal 1 wire bus.
-    @note   This only supports a single instance of a 1 wire bus.
-*/
-
-#include <stdio.h>
-
-#include <stdint.h>
 #include "mbed.h"
-// #include "delay.h"
 #include "u1wire.h"
-// #include "irq.h"
-// #include "pio.h"
 
 enum {
     U1WIRE_READ_ROM = 0x33,
@@ -26,116 +13,116 @@ enum {
     U1WIRE_ADDR_BYTES = 6
 };
 
-#define U1WIRE_DEBUG 1
+// #define U1WIRE_DEBUG 1
 
-#ifndef U1WIRE_PIO
-#error U1WIRE_PIO needs to be defined in target.h
-#endif
+// #ifndef U1WIRE_PIO
+// #error U1WIRE_PIO needs to be defined in target.h
+// #endif
 
-#define U1WIRE_RELEASE() pio_config_set(U1WIRE_PIO, PIO_CONFIG_PULLUP)
+// #define U1WIRE_RELEASE() pio_config_set(U1WIRE_PIO, PIO_CONFIG_PULLUP)
 
-#define U1WIRE_DRIVE() pio_config_set(U1WIRE_PIO, PIO_CONFIG_OUTPUT_LOW)
+// #define U1WIRE_DRIVE() pio_config_set(U1WIRE_PIO, PIO_CONFIG_OUTPUT_LOW)
 
-#define U1WIRE_TEST() pio_input_get(U1WIRE_PIO)
+// #define U1WIRE_TEST() pio_input_get(U1WIRE_PIO)
 
 /** Reset one wire bus.
     @return 1 if OK, 0 if no device responding, negative if error  */
 int8_t u1wire_reset(void) {
-    /* Let bus float high by tristating driver.  Note need 4.7 kohm
-       pullup resistor.  Internal pullup usually too weak.  I have
-       found that without the pullup resistor we can detect a device
-       but not on the first attempt.  */
-    U1WIRE_RELEASE();
-    DELAY_US(5);
+    // /* Let bus float high by tristating driver.  Note need 4.7 kohm
+    //    pullup resistor.  Internal pullup usually too weak.  I have
+    //    found that without the pullup resistor we can detect a device
+    //    but not on the first attempt.  */
+    // U1WIRE_RELEASE();
+    // wait_us(5);
 
-    if (!U1WIRE_TEST()) {
-        /* Bus won't go high.  */
-        return -U1WIRE_ERR_BUS_LOW;
-    }
+    // if (!U1WIRE_TEST()) {
+    //     /* Bus won't go high.  */
+    //     return -U1WIRE_ERR_BUS_LOW;
+    // }
 
-    irq_global_disable();
+    // irq_global_disable();
 
-    /* Need to drive bus low for at least 480 microseconds.  We drive
-       it for 250 microseconds, check that it is low, then wait
-       another 250 microseconds.  This is called the reset pulse.  */
-    U1WIRE_DRIVE();
-    DELAY_US(250);
+    // /* Need to drive bus low for at least 480 microseconds.  We drive
+    //    it for 250 microseconds, check that it is low, then wait
+    //    another 250 microseconds.  This is called the reset pulse.  */
+    // U1WIRE_DRIVE();
+    // DELAY_US(250);
 
-    if (U1WIRE_TEST()) {
-        U1WIRE_RELEASE();
-        irq_global_enable();
-        /* Bus won't go low.  */
-        return -U1WIRE_ERR_BUS_HIGH;
-    }
+    // if (U1WIRE_TEST()) {
+    //     U1WIRE_RELEASE();
+    //     irq_global_enable();
+    //     /* Bus won't go low.  */
+    //     return -U1WIRE_ERR_BUS_HIGH;
+    // }
 
-    DELAY_US(250);
+    // DELAY_US(250);
 
-    /* Let bus float high by tristating driver.  */
-    U1WIRE_RELEASE();
+    // /* Let bus float high by tristating driver.  */
+    // U1WIRE_RELEASE();
 
-    DELAY_US(10);
-    if (!U1WIRE_TEST()) {
-        irq_global_enable();
-        /* Bus stuck low.  */
-        return -U1WIRE_ERR_BUS_STUCK;
-    }
+    // DELAY_US(10);
+    // if (!U1WIRE_TEST()) {
+    //     irq_global_enable();
+    //     /* Bus stuck low.  */
+    //     return -U1WIRE_ERR_BUS_STUCK;
+    // }
 
-    /* The rising edge should cause the slave to respond between
-       15--60 microseconds later.  It should then drive the bus low
-       for 60--240 microseconds.  */
-    DELAY_US(60);
+    // /* The rising edge should cause the slave to respond between
+    //    15--60 microseconds later.  It should then drive the bus low
+    //    for 60--240 microseconds.  */
+    // DELAY_US(60);
 
-    if (U1WIRE_TEST()) {
-        irq_global_enable();
-        /* No device responding.  */
-        return 0;
-    }
+    // if (U1WIRE_TEST()) {
+    //     irq_global_enable();
+    //     /* No device responding.  */
+    //     return 0;
+    // }
 
-    DELAY_US(10);
-    if (U1WIRE_TEST()) {
-        irq_global_enable();
-        /* Slave does not drive bus long enough.  */
-        return -U1WIRE_ERR_PRESENCE_SHORT;
-    }
+    // DELAY_US(10);
+    // if (U1WIRE_TEST()) {
+    //     irq_global_enable();
+    //     /* Slave does not drive bus long enough.  */
+    //     return -U1WIRE_ERR_PRESENCE_SHORT;
+    // }
 
-    DELAY_US(240);
-    if (!U1WIRE_TEST()) {
-        irq_global_enable();
-        /* Slave still driving bus longer than it should.  */
-        return -U1WIRE_ERR_PRESENCE_LONG;
-    }
+    // DELAY_US(240);
+    // if (!U1WIRE_TEST()) {
+    //     irq_global_enable();
+    //     /* Slave still driving bus longer than it should.  */
+    //     return -U1WIRE_ERR_PRESENCE_LONG;
+    // }
 
-    /* The read slot must be a minimum of 480 microseconds.  */
-    DELAY_US(480 - 240);
-    irq_global_enable();
+    // /* The read slot must be a minimum of 480 microseconds.  */
+    // DELAY_US(480 - 240);
+    // irq_global_enable();
     return 1;
 }
 
 /** Write a bit on the one wire bus.
     @param value bit to write  */
 void u1wire_bit_write(uint8_t value) {
-    irq_global_disable();
+    // irq_global_disable();
 
-    /* 1 wire devices sample the bus somewhere between 15--60
-       microseconds after the bus being pulled low.
-       I've measured a 1 bit being low for 10 microseconds
-       and a 0 bit being low for 70 microseconds.  */
+    // /* 1 wire devices sample the bus somewhere between 15--60
+    //    microseconds after the bus being pulled low.
+    //    I've measured a 1 bit being low for 10 microseconds
+    //    and a 0 bit being low for 70 microseconds.  */
 
-    U1WIRE_DRIVE();
-    DELAY_US(10 - U1WIRE_DELAY_OFFSET);
+    // U1WIRE_DRIVE();
+    // DELAY_US(10 - U1WIRE_DELAY_OFFSET);
 
-    /* When writing a 1 bit, the bus must be released within 15
-       microseconds of pulling the bus low.  */
-    if (value)
-        U1WIRE_RELEASE();
+    // /* When writing a 1 bit, the bus must be released within 15
+    //    microseconds of pulling the bus low.  */
+    // if (value)
+    //     U1WIRE_RELEASE();
 
-    /* Write slots must be a minimum of 60 microseconds in length.  */
-    DELAY_US(60 - U1WIRE_DELAY_OFFSET);
-    U1WIRE_RELEASE();
+    // /* Write slots must be a minimum of 60 microseconds in length.  */
+    // DELAY_US(60 - U1WIRE_DELAY_OFFSET);
+    // U1WIRE_RELEASE();
 
-    /* Need a recovery time of at least 1 microsecond between write pulses.
-       The function call/return time will exceed this.  */
-    irq_global_enable();
+    // /* Need a recovery time of at least 1 microsecond between write pulses.
+    //    The function call/return time will exceed this.  */
+    // irq_global_enable();
 }
 
 /** Write a byte on the one wire bus.
@@ -155,24 +142,24 @@ void u1wire_byte_write(uint8_t value) {
 uint8_t u1wire_bit_read(void) {
     uint8_t value;
 
-    irq_global_disable();
+    // irq_global_disable();
 
-    /* Generate a read pulse; need to drive the bus for at least 1
-       microsecond.  */
-    U1WIRE_DRIVE();
-    DELAY_US(2 - 1);
-    U1WIRE_RELEASE();
+    // /* Generate a read pulse; need to drive the bus for at least 1
+    //    microsecond.  */
+    // U1WIRE_DRIVE();
+    // DELAY_US(2 - 1);
+    // U1WIRE_RELEASE();
 
-    DELAY_US(12 - U1WIRE_DELAY_OFFSET);
-    /* The data must be read within 15 microseconds from the start of
-       the slot.  */
-    value = U1WIRE_TEST();
+    // DELAY_US(12 - U1WIRE_DELAY_OFFSET);
+    // /* The data must be read within 15 microseconds from the start of
+    //    the slot.  */
+    // value = U1WIRE_TEST();
 
-    /* All read slots must be a minimum of 60 microseconds duration with a
-       recovery time of at least 1 microsecond between read pulses.  */
-    DELAY_US(60);
+    // /* All read slots must be a minimum of 60 microseconds duration with a
+    //    recovery time of at least 1 microsecond between read pulses.  */
+    // DELAY_US(60);
 
-    irq_global_enable();
+    // irq_global_enable();
     return value;
 }
 
@@ -285,7 +272,7 @@ bool u1wire_ready_p(void) {
     @param device pointer to one wire device structure
     @return 1 if OK, 0 if no device responding, negative if error  */
 int8_t u1wire_init(u1wire_obj_t *device) {
-    U1WIRE_RELEASE();
+    // U1WIRE_RELEASE();
 
     return u1wire_rom_code_read(device);
 }
--- source/src/modules/1wire/u1wire.h
+++ source/src/modules/1wire/u1wire.h
@@ -1,27 +1,10 @@
-/** @file   u1wire.h
-    @author M. P. Hayes, UCECE
-    @date   08 June 2002
-    @brief  One wire bus driver.
-    @note  This interface only supports the one bus but with multiple devices
-  on the bus.  This was a deliberate choice that I may regret one day.
-  It keeps the interface simpler, reduces the code size, and most
-  systems will only ever have a single one wire bus.  To support multiple
-  one wire buses then run-time configuration will need to be passed
-  as an additional argument to u1wire_init.
-*/
-
-#ifndef U1WIRE_H
-#define U1WIRE_H
-
-#ifdef __cplusplus
-extern "C"
-{
-#endif
+#ifndef _NS_U1WIRE_H
+#define _NS_U1WIRE_H
 
 #include <stdint.h>
 
 /** One wire bus error codes.  */
-typedef enum u1wire_err {
+typedef enum {
     U1WIRE_ERR_BUS_LOW = 1,        /* Can't drive bus high.  */
     U1WIRE_ERR_BUS_STUCK = 2,      /* Bus stuck low.  */
     U1WIRE_ERR_PRESENCE_SHORT = 3, /* Slave presence pulse too short.  */
@@ -52,72 +35,57 @@ typedef u1wire_obj_t *u1wire_t;
 
 /** Determine if one wire bus operation has completed.
 @return 1 if one wire bus operation has completed otherwise 0  */
-extern bool
-u1wire_ready_p(void);
+uint8_t u1wire_ready_p(void);
 
 /** Reset one wire bus.
 @return 1 if OK, 0 if no device responding, negative if error  */
-extern int8_t
-u1wire_reset(void);
+int8_t u1wire_reset(void);
 
 /** Write a bit on the one wire bus.
 @param value bit to write  */
-extern void
-u1wire_bit_write(uint8_t value);
+void u1wire_bit_write(uint8_t value);
 
 /** Write a byte on the one wire bus.
 @param value byte to write.  */
-extern void
-u1wire_byte_write(uint8_t value);
+void u1wire_byte_write(uint8_t value);
 
 /** Read a bit from the one wire bus.
 @return bit read  */
-extern uint8_t
-u1wire_bit_read(void);
+uint8_t u1wire_bit_read(void);
 
 /** Read a byte from the one wire bus.
 @return byte read  */
-extern uint8_t
-u1wire_byte_read(void);
+uint8_t u1wire_byte_read(void);
 
 /** Send command to selected device on one wire bus.
 @param dev pointer to one wire device structure
 @param command command to send
 @return 1 if OK, 0 if no device responding, negative if error  */
-extern int8_t
-u1wire_command(u1wire_t dev, uint8_t command);
+int8_t u1wire_command(u1wire_t dev, uint8_t command);
 
 /** Broadcast command to all devices on one wire bus.
 @param command command to send
 @return 1 if OK, 0 if no device responding, negative if error  */
-extern int8_t
-u1wire_broadcast(uint8_t command);
+int8_t u1wire_broadcast(uint8_t command);
 
 /** Read number of bytes of data from one wire bus.
 @param data pointer to buffer to write to
 @param size number of bytes to write
 @return number of bytes read, 0 if no device responding, negative if error  */
-extern int8_t
-u1wire_read(void *data, uint8_t size);
+int8_t u1wire_read(void *data, uint8_t size);
 
 /** Write number of bytes of data from one wire bus.
 @param data pointer to buffer to write to
 @param size number of bytes to write
 @return number of bytes written, 0 if no device responding, negative if error  */
-extern int8_t
-u1wire_write(void *data, uint8_t size);
+int8_t u1wire_write(void *data, uint8_t size);
 
 /** Initialise one wire bus and discover if there is a single device on the bus.
 @param device pointer to one wire device structure
 @return 1 if OK, 0 if no device responding, negative if error  */
-extern int8_t
-u1wire_init(u1wire_obj_t *device);
+int8_t u1wire_init(u1wire_obj_t *device);
 
 /* In u1wire_debug.c  */
-extern void
-u1wire_debug(u1wire_t dev);
+void u1wire_debug(u1wire_t dev);
 
-#ifdef __cplusplus
-}
-#endif
 #endif
